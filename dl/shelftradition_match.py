737aeda8a553545c94c502e7086433d00feefc03
huachao2017 <21006735@qq.com>
2019-09-19 19:04:41 +0800

    - 修订模式识别的接口

--- dl/shelftradition_match.py
+++ dl/shelftradition_match.py
@@ -9,21 +9,15 @@ import numpy as np
 logger = logging.getLogger("django")
 
 class ShelfTraditionMatch:
-    def __init__(self, shopid, shelfid):
-        self.added_sample_upcs_to_cnt = {}
+    def __init__(self, upc):
         self._matcher = Matcher(visual=True)
 
-        logger.info('begin loading TraditionMatch:{},{}'.format(shopid,shelfid))
-        base_samples = ShelfGoods.objects.filter(shopid=shopid).filter(shelfid=shelfid).filter(score2=1)
-        sample_dir = os.path.join(settings.MEDIA_ROOT, settings.DETECT_DIR_NAME, 'shelf_sample',
-                                  '{}'.format(shopid),
-                                  '{}'.format(shelfid))
-        for sample in base_samples:
-            sample_image_path = os.path.join(sample_dir, '{}_{}.jpg'.format(sample.upc, sample.pk))
+        logger.info('begin loading TraditionMatch:{}'.format(upc))
+        sample_dir = os.path.join(settings.MEDIA_ROOT, settings.DETECT_DIR_NAME, 'shelf_sample','{}'.format(upc))
+        for sample in os.listdir(sample_dir):
+            sample_image_path = os.path.join(sample_dir, sample)
             if os.path.isfile(sample_image_path):
-                self._matcher.add_baseline_image(sample_image_path, sample.upc) # 基准样本不能被删除，所以直接调用_matcher的方法
-            else:
-                ShelfGoods.objects.get(pk=sample.pk).delete()
+                self._matcher.add_baseline_image(sample_image_path, upc) # 基准样本不能被删除，所以直接调用_matcher的方法
 
         logger.info('end loading TraditionMatch:{}'.format(self._matcher.get_baseline_cnt()))
 
@@ -31,10 +25,10 @@ class ShelfTraditionMatch:
         upc, score = self._matcher.match_image_best_one(image_path)
         return upc, score
 
-    def detect_one_with_cv2array(self,visual_image_path,image):
-        upc, score = self._matcher.match_image_best_one_with_cv2array(visual_image_path, cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR))
+    def detect_one_with_cv2array(self,visual_image_path,cv2image):
+        upc, score = self._matcher.match_image_best_one_with_nparray(visual_image_path, cv2.cvtColor(np.array(cv2image),cv2.COLOR_RGB2BGR))
         return upc, score
 
-    def verify_score(self,image_path,within_upcs):
-        upc, score = self._matcher.match_image_best_one(image_path,within_upcs=within_upcs)
-        return score
+    def detect_one_with_nparray(self,visual_image_path,npimage):
+        upc, score = self._matcher.match_image_best_one_with_nparray(visual_image_path, np.array(npimage))
+        return upc, score
