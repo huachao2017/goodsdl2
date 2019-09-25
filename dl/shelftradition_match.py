997615b8fa9fec40efbdfb1859db708bc96c656a
huachao2017 <21006735@qq.com>
2019-09-25 17:52:57 +0800

    - 防止抛出错误

--- dl/shelftradition_match.py
+++ dl/shelftradition_match.py
@@ -13,6 +13,7 @@ class ShelfTraditionMatch:
     def __init__(self, upc):
         self._matcher = Matcher(visual=True)
         self._upc = upc
+        self._init = True
         self.visual_image_path = os.path.join(settings.MEDIA_ROOT, settings.DETECT_DIR_NAME, 'shelf_goods_visual',
                                          '{}'.format(self._upc))
         if not tf.gfile.Exists(self.visual_image_path):
@@ -23,14 +24,20 @@ class ShelfTraditionMatch:
 
         logger.info('begin loading TraditionMatch:{}'.format(upc))
         sample_dir = os.path.join(settings.MEDIA_ROOT, settings.DETECT_DIR_NAME, 'shelf_sample','{}'.format(upc))
-        for sample in os.listdir(sample_dir):
-            sample_image_path = os.path.join(sample_dir, sample)
-            if os.path.isfile(sample_image_path):
-                self._matcher.add_baseline_image(sample_image_path, upc) # 基准样本不能被删除，所以直接调用_matcher的方法
+        if not os.path.isdir(sample_dir):
+            self._init = False
+
+        if self._init:
+            for sample in os.listdir(sample_dir):
+                sample_image_path = os.path.join(sample_dir, sample)
+                if os.path.isfile(sample_image_path):
+                    self._matcher.add_baseline_image(sample_image_path, upc) # 基准样本不能被删除，所以直接调用_matcher的方法
 
         logger.info('end loading TraditionMatch:{}'.format(self._matcher.get_baseline_cnt()))
 
     def detect_one_with_path(self,image_path):
+        if not self._init:
+            return None
         upc, cnt = self._matcher.match_result(image_path)
         if upc == self._upc:
             return True
@@ -41,6 +48,8 @@ class ShelfTraditionMatch:
                 return None
 
     def detect_one_with_cv2array(self,cv2image):
+        if not self._init:
+            return None
         upc, cnt = self._matcher.match_image_best_one_with_nparray(self.visual_image_path, cv2.cvtColor(np.array(cv2image),cv2.COLOR_RGB2BGR))
         if upc == self._upc:
             return True
@@ -52,6 +61,8 @@ class ShelfTraditionMatch:
 
 
     def detect_one_with_nparray(self,npimage):
+        if not self._init:
+            return None
         upc, cnt = self._matcher.match_image_best_one_with_nparray(self.visual_image_path, np.array(npimage))
         if upc == self._upc:
             return True
