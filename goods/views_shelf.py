aa92e6715402d8e473a666de8bf5cc430faed868
huachao2017 <21006735@qq.com>
2019-09-29 10:48:58 +0800

    - fix bug

--- goods/views_shelf.py
+++ goods/views_shelf.py
@@ -434,19 +434,22 @@ class ShelfGoodsViewSet(DefaultMixin, mixins.CreateModelMixin, mixins.ListModelM
                 serializer.instance.shelf_image.pk,
                 serializer.instance.pk)
 
-            # 添加新样本
-            sample_dir = os.path.join(settings.MEDIA_ROOT, settings.DETECT_DIR_NAME, 'shelf_sample')
-            if serializer.instance.shelf_image.rectsource != '':
-                image_path = os.path.join(settings.MEDIA_ROOT, serializer.instance.shelf_image.rectsource)
+            if upc is not None and upc != '':
+                # 添加新样本
+                sample_dir = os.path.join(settings.MEDIA_ROOT, settings.DETECT_DIR_NAME, 'shelf_sample')
+                if serializer.instance.shelf_image.rectsource != '':
+                    image_path = os.path.join(settings.MEDIA_ROOT, serializer.instance.shelf_image.rectsource)
+                else:
+                    image_path = os.path.join(settings.MEDIA_ROOT, serializer.instance.shelf_image.source)
+                image = PILImage.open(image_path)
+                sample_image = image.crop((serializer.instance.xmin, serializer.instance.ymin, serializer.instance.xmax, serializer.instance.ymax))
+                sample_image_path = os.path.join(sample_dir, upc, '{}.jpg'.format(serializer.instance.pk))
+                logger.info("add upc image ,filepath="+str(sample_image_path))
+                sample_image.save(sample_image_path, 'JPEG',quality=100)
+                serializer.instance.upc = upc
+                serializer.instance.save()
             else:
-                image_path = os.path.join(settings.MEDIA_ROOT, serializer.instance.shelf_image.source)
-            image = PILImage.open(image_path)
-            sample_image = image.crop((serializer.instance.xmin, serializer.instance.ymin, serializer.instance.xmax, serializer.instance.ymax))
-            sample_image_path = os.path.join(sample_dir, upc, '{}.jpg'.format(serializer.instance.pk))
-            logger.info("add upc image ,filepath="+str(sample_image_path))
-            sample_image.save(sample_image_path, 'JPEG',quality=100)
-            serializer.instance.upc = upc
-            serializer.instance.save()
+                logger.error('get_upc is None')
 
         # compare_ret = tz_good_compare.level_compare(
         #     serializer.instance.shelf_image.displayid,
