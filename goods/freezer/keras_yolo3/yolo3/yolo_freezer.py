78419514ee8eef9d487269d60101c675f89b9c2d
LRJliurj <908601417@qq.com>
2019-10-11 14:04:25 +0800

    yolov3

--- goods/freezer/keras_yolo3/yolo3/yolo_freezer.py
+++ goods/freezer/keras_yolo3/yolo3/yolo_freezer.py
@@ -24,11 +24,11 @@ import logging
 import tensorflow as tf
 # from keras.backend.tensorflow_backend import set_session,get_session
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-config1 = tf.ConfigProto()
-config1.gpu_options.allow_growth=True   #不全部占满显存, 按需分配
-config1.gpu_options.per_process_gpu_memory_fraction = 0.3
-K.set_session(tf.Session(config=config1))
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# config1 = tf.ConfigProto()
+# config1.gpu_options.allow_growth=True   #不全部占满显存, 按需分配
+# config1.gpu_options.per_process_gpu_memory_fraction = 0.3
+# K.set_session(tf.Session(config=config1))
 
 # set_session(tf.Session(config=config1))
 logger = logging.getLogger("detect")
