ceecba9d560d6c8fd5722a72a96d954993725bbe
TreesLi <wlgcxy2012@163.com>
2019-10-15 15:47:51 +0800

    可根据upc管理图库

--- goods/shelfgoods/imgsearch/baidu_ai/search.py
+++ goods/shelfgoods/imgsearch/baidu_ai/search.py
@@ -17,14 +17,15 @@ class ImgSearch_02:
     sleep_time = baidu_ai_instance['sleep_time']
     ak = baidu_ai_instance['ak']
     sk = baidu_ai_instance['sk']
+    first_token = True
 
     def add_img(self, upc, imgname, img_path):
         for i in range(1, 5):
             try:
                 with open(img_path, 'rb') as f:
                     base64_data = base64.b64encode(f.read())
-                brief = "{'upc':%s, 'id':%s}" % (str(upc), str(imgname))
-                params = {"brief": brief, "image": base64_data}
+                # brief = "{'upc':%s, 'id':%s}" % (str(upc), str(imgname))
+                params = {"brief": upc, "image": base64_data}
                 params = parse.urlencode(params).encode("utf-8")
                 request_url = self.request_url + "add?access_token=" + self.get_token()
                 req = request.Request(url=request_url, data=params)
@@ -53,8 +54,8 @@ class ImgSearch_02:
                 img_encode = cv2.imencode('.jpg', cvimg)[1]
                 base64_data = base64.b64encode(img_encode)
 
-                brief = "{'upc':%s, 'id':%s}" % (str(upc), str(imgname))
-                params = {"brief": brief, "image": base64_data}
+                # brief = "{'upc':%s, 'id':%s}" % (str(upc), str(imgname))
+                params = {"brief": upc, "image": base64_data}
                 params = parse.urlencode(params).encode("utf-8")
                 request_url = self.request_url + "add?access_token=" + self.get_token()
                 req = request.Request(url=request_url, data=params)
@@ -120,7 +121,8 @@ class ImgSearch_02:
                     # print(score)
                     if score > self.min_score:
                         try:
-                            upcs.append(str(eval(result["brief"])["upc"]))
+                            # upcs.append(str(eval(result["brief"])["upc"]))
+                            upcs.append(str(result["brief"]))
                         except:
                             continue
                     else:
@@ -155,7 +157,8 @@ class ImgSearch_02:
                     score = result["score"]
                     if score > self.min_score:
                         try:
-                            upcs.append(str(eval(result["brief"])["upc"]))
+                            # upcs.append(str(eval(result["brief"])["upc"]))
+                            upcs.append(str(result["brief"]))
                         except:
                             continue
                     else:
@@ -187,9 +190,11 @@ class ImgSearch_02:
                 upc =None
                 for result in results:
                     score = result["score"]
+                    print(score)
                     if score > self.min_score_top1:
                         try:
-                            upc=str(eval(result["brief"])["upc"])
+                            # upc=str(eval(result["brief"])["upc"])
+                            upc=str(result["brief"])
                             break
                         except:
                             continue
@@ -201,32 +206,41 @@ class ImgSearch_02:
                 time.sleep(self.sleep_time)
                 continue
         return None,code.code_20
+
     def get_token(self):
-        for i in range(1, 5):
-            try:
-                # client_id 为官网获取的AK， client_secret 为官网获取的SK
-                host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={}&client_secret={}'.format(self.ak,self.sk)
-                req = request.Request(host)
-                req.add_header('Content-Type', 'application/json; charset=UTF-8')
-                response = request.urlopen(req)
-                content = response.read().decode("utf-8")
-                content = json.loads(content)
-                access_token = content["access_token"]
-                print(access_token)
-                return access_token
-            except Exception as err:
-                logging.error(err)
-                time.sleep(0.1)
-                continue
-        return None
+        start = time.time()
+        if not self.first_token:
+            return "24.291e9753f7fb84d43017ee8e8cd23587.2592000.1573636360.282335-17508601"
+        else:
+            for i in range(1, 5):
+                try:
+                    # client_id 为官网获取的AK， client_secret 为官网获取的SK
+                    host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={}&client_secret={}'.format(self.ak,self.sk)
+                    req = request.Request(host)
+                    req.add_header('Content-Type', 'application/json; charset=UTF-8')
+                    response = request.urlopen(req)
+                    content = response.read().decode("utf-8")
+                    content = json.loads(content)
+                    access_token = content["access_token"]
+                    print(access_token)
+                    end = time.time()
+                    print(end - start)
+                    return access_token
+                except Exception as err:
+                    logging.error(err)
+                    time.sleep(0.1)
+                    continue
+            return None
 
 if __name__ == '__main__':
     demo = ImgSearch_02()
 
     # print(demo.add_img(100,'3567_456',"/Users/86130/Desktop/upc_test2/6902538005141_40754.jpg"))
     # print(demo.delete_img("1441064327,3222327728;1161903065,2930872941"))
-    # print(demo.search_cvimg_top1("/Users/86130/Desktop/upc_test2/002.png"))
-
+    # start = time.time()
+    # print(demo.search_img("/Users/86130/Desktop/upc_test2/003.png"))
+    # end = time.time()
+    # print(end-start)
     img = cv2.imread("/Users/86130/Desktop/upc_test2/6902538005141_40786.jpg")
-    print(demo.search_cvimg_top1(img))
+    print(demo.search_cvimg(img))
 
