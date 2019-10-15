da9b62868c21744d2df14c2e28fad97be05f4d70
LRJliurj <908601417@qq.com>
2019-10-15 11:17:48 +0800

    bigdata环境搭建

---
+++ goods/sellgoods/salesquantity/utils/sparkdb.py
@@ -0,0 +1,33 @@
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+from set_config import config
+#
+# from sellgoods.sql import sales_quantity
+# spark_context = config.shellgoods_params['spark_context']
+db_context = config.db_context
+# db_context = config.db_context
+class SparkDb:
+    def get_spark_context(self):
+        # sc = SparkContext(spark_context, sc_name)
+        # sc = SparkSession.builder.master(spark_context).appName(self.sc_name).config('spark.executor.memory','2g').getOrCreate()
+        # sc = ss.sparkContext()
+        sc = SparkContext()
+        sc.setSystemProperty('spark.executor.memory', '8g')
+        sc.setSystemProperty('spark.driver.memory', '6g')
+        sc.setSystemProperty('spark.worker.memory', '8g')
+        sc.setSystemProperty('spark.driver.maxResultsSize', '0')
+        sc.setSystemProperty('spark.executor.cores', '6')
+        sc.setSystemProperty('spark.shuffle.memoryFraction', '0')
+        sc.setSystemProperty("spark.sql.execution.arrow.enabled", "true")
+        return sc
+    def get_sparksql_context(self,sc):
+        sqlContext = SQLContext(sc)
+        return sqlContext
+    def  get_data_frame(self,sql,sqlContext):
+        df = sqlContext.read.format("jdbc").options(url=db_context["url"],
+                                                        driver=db_context["driver"],
+                                                        # dbtable="(SELECT code,title,description FROM project) tmp",
+                                                        dbtable=sql,
+                                                        user=db_context["user"], password=db_context["password"]).load()
+        return df
---
+++ goods/sellgoods/salesquantity/utils/test_spark/test1.py
@@ -0,0 +1,19 @@
+from goods.sellgoods.salesquantity.utils import sparkdb
+from goods.sellgoods.sql import sales_quantity
+sqldemo = sales_quantity.sql_params['linear_regression']
+
+def exe():
+    sc_name = "app222"
+    db = sparkdb.SparkDb(sc_name)
+    sc = db.get_spark_context()
+    # sc = db.get_spark_context()
+    # df = db.get_data_frame("(select sum(number) as nums,shop_id,shop_goods_id from payment_detail "
+    #                     "where shop_id is not null and shop_goods_id is not null and number > 0 and "
+    #                     "payment_id in ( "
+    #                      "select payment.id from payment where payment.type != 50)  tmp")
+    df = db.get_data_frame(sqldemo,sc)
+    print (df.collect())
+    return df,sc
+
+if __name__=="__main__":
+    exe()
