a51bd6e16a4838115cf8865f9180213d6619fb53
huachao2017 <21006735@qq.com>
2019-10-18 11:38:17 +0800

    - 修订

--- goods/models.py
+++ goods/models.py
@@ -85,7 +85,7 @@ class FirstGoodsSelection(models.Model):
     code = models.CharField(max_length=20)
     predict_sales_amount = models.IntegerField()
     template_shop_ids = models.CharField(max_length=100)
-    mch_goods_code = models.CharField(max_length=100)
+    mch_goods_code = models.CharField(max_length=100,default='')
     mch_code = models.IntegerField(default=2)
 
 class ai_sales_goods(models.Model):
