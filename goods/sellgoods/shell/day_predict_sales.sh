fcc71521ad1e47087d5d03e6e16e8baceb171dd3
LRJliurj <908601417@qq.com>
2019-10-19 11:33:31 +0800

    生成订单

--- goods/sellgoods/shell/day_predict_sales.sh
+++ goods/sellgoods/shell/day_predict_sales.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+source /etc/profile
 today=`date +"%Y-%m-%d"`
 nohup python3 /home/src/goodsdl2/goods/sellgoods/salesquantity/service/day_train_service.py >> /home/ai/logs/sales_predict.log""$today 2>&1 &
