3303f601e4a65e20f7bef24b6c74c661c10309ab
LRJliurj <908601417@qq.com>
2019-10-28 14:14:57 +0800

    添加最小起订量规则 和 上下限规则

--- goods/sellgoods/salesquantity/local_util/stock_util1.py
+++ goods/sellgoods/salesquantity/local_util/stock_util1.py
@@ -146,7 +146,7 @@ def get_min_sku(shelf_good_infos,shelf_infos,tz_ids,mysql_ins):
                 if str(upc) != 'undefined' and str(upc) != '' :
                     sql_upc_depth = sales_quantity.sql_params["upc_mch_code"]
                     sql_upc_depth = sql_upc_depth.format(upc,mch_id,mch_goods_code)
-                    # print (sql_upc_depth)
+                    print (sql_upc_depth)
                     upc_result = mysql_ins.selectOne(sql_upc_depth)
                     if upc_result is not None:
                         upc1_depth = upc_result[4]
