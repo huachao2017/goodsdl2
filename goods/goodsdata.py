ccf3723c8f1092fd3c220d10ee2df14b0c3c7dd3
huachao2017 <21006735@qq.com>
2019-10-30 13:52:56 +0800

    - 获取台账陈列的商品类别

--- goods/goodsdata.py
+++ goods/goodsdata.py
@@ -13,40 +13,45 @@ django.setup()
 from django.db import connections
 
 
-def get_shop_shelfs(shopid):
+def get_raw_shop_shelfs(shopid):
     """
-    获取商店的所有货架及货架的相关信息
+    获取商店的所有货架及货架的相关信息，该方法在陈列时用
     :param shopid: fx系统的商店id
-    :return:返回一个DataShop对象
+    :return:返回一个DataRawShelf对象列表
     """
 
-    data_shop = DataShop(shopid)
+    ret = []
     cursor = connections['ucenter'].cursor()
     # 获取台账系统的uc_shopid
     cursor.execute('select id, mch_id from uc_shop where mch_shop_code = {}'.format(shopid))
     (uc_shopid, mch_id) = cursor.fetchone()
 
     # 获取台账
-    # TODO 需要获取商店台账可放的品类
-    cursor.execute("select t.id, t.shelf_id, t.shelf_count from sf_shop_taizhang st, sf_taizhang t where st.taizhang_id=t.id and st.shop_id = {}".format(uc_shopid))
+    cursor.execute("select t.id, t.shelf_id, t.shelf_count from sf_shop_taizhang st, sf_taizhang t, sf_taizhang_display td where st.taizhang_id=t.id and td.taizhang_id=t.id and td.status=1 and st.shop_id = {}".format(uc_shopid))
     taizhangs = cursor.fetchall()
     for taizhang in taizhangs:
         taizhang_id = taizhang[0]
         shelf_id = taizhang[1]
         count = taizhang[2]
+        # 获取商店台账可放的品类
+        try:
+            cursor.execute("select associated_catids from sf_taizhang_display where taizhang_id = {} and status in (0,1) and approval_status = 0".format(taizhang_id))
+            (associated_catids,) = cursor.fetchone()
+        except:
+            print('获取台账成列失败：{}！'.format(taizhang_id))
+            associated_catids = None
         cursor.execute("select t.shelf_no,s.length,s.height,s.depth from sf_shelf s, sf_shelf_type t where s.shelf_type_id=t.id and s.id={}".format(shelf_id))
         (shelf_no, length, height, depth) = cursor.fetchone()
-        for i in range(count):
-            data_shelf = DataShelf(taizhang_id, shelf_id, shelf_no,length,height,depth)
-            data_shop.add_data_shelf(data_shelf)
+        data_raw_shelf = DataRawShelf(taizhang_id, shelf_id, shelf_no,count, length,height,depth,associated_catids)
+        ret.append(data_raw_shelf)
 
     cursor.close()
 
-    return data_shop
+    return ret
 
 def get_raw_goods_info(shopid, mch_codes):
     """
-    根据upc列表获取每个upc的类别、长宽高
+    根据upc列表获取每个upc的类别、长宽高，该方法在陈列时用
     :param shopid: fx系统的商店id
     :param mch_codes:
     :return:返回一个DataRawGoods对象的列表
@@ -106,19 +111,19 @@ def get_raw_goods_info(shopid, mch_codes):
 
 def get_shop_shelf_goods(shopid):
     """
-    获取商店的所有货架及货架上的商品信息
+    获取商店的所有货架及货架上的商品信息，该方法在订货时用
     :param shopid: fx系统的商店id
-    :return:返回一个DataShop对象
+    :return:返回DataShelf列表
     """
 
-    data_shop = DataShop(shopid)
+    ret = []
     cursor = connections['ucenter'].cursor()
     # 获取台账系统的uc_shopid
     cursor.execute('select id, mch_id from uc_shop where mch_shop_code = {}'.format(shopid))
     (uc_shopid, mch_id) = cursor.fetchone()
 
     # 获取台账 TODO 只能获取店相关的台账，不能获取商家相关的台账
-    cursor.execute("select t.id, t.shelf_id, td.display_shelf_info, td.display_goods_info from sf_shop_taizhang st, sf_taizhang t, sf_taizhang_display td where st.taizhang_id=t.id and td.taizhang_id=t.id and td.status=2 and st.shop_id = {}".format(uc_shopid))
+    cursor.execute("select t.id, t.shelf_id, td.display_shelf_info, td.display_goods_info from sf_shop_taizhang st, sf_taizhang t, sf_taizhang_display td where st.taizhang_id=t.id and td.taizhang_id=t.id and td.status in (1,2) and td.approval_status=1 and st.shop_id = {}".format(uc_shopid))
     taizhangs = cursor.fetchall()
     for taizhang in taizhangs:
         taizhang_id = taizhang[0]
@@ -141,7 +146,7 @@ def get_shop_shelf_goods(shopid):
             level_array = shelf_dict[shelfId]
             goods_array = goods_array_dict[shelfId]
             data_shelf = DataShelf(taizhang_id, shelf_id, shelf_no, length, height, depth)
-            data_shop.add_data_shelf(data_shelf)
+            ret.append(data_shelf)
             for i in range(len(level_array)):
                 level = level_array[i]
                 goods_level_array = goods_array[i]
@@ -161,20 +166,19 @@ def get_shop_shelf_goods(shopid):
     return data_shop
 
 
-class DataShop():
-    def __init__(self, shopid):
-        self.shopid = shopid
-        self.data_shelfs = []
-
-    def add_data_shelf(self, data_shelf):
-        self.data_shelfs.append(data_shelf)
+class DataRawShelf():
+    def __init__(self, taizhang_id, shelf_id, type, count, length, height, depth, associated_catids):
+        self.taizhang_id = taizhang_id
+        self.shelf_id = shelf_id
+        self.type = type
+        self.count = count
+        self.length = length
+        self.height = height
+        self.depth = depth
+        self.associated_catids = associated_catids
 
     def __str__(self):
-        ret = '{}:[\n'.format(self.shopid)
-        for data_shelf in self.data_shelfs:
-            ret += str(data_shelf)
-            ret += '\n'
-        ret += ']'
+        ret = '{},{},{},{},{},{},{},{}'.format(self.taizhang_id, self.shelf_id, self.type, self.count, self.length, self.height, self.depth, self.associated_catids)
         return ret
 
 class DataShelf():
@@ -191,13 +195,13 @@ class DataShelf():
         self.data_levels.append(data_level)
 
     def __str__(self):
-        ret = '\t{},{},{},{},{},{}'.format(self.taizhang_id, self.shelf_id, self.type, self.length, self.height, self.depth)
+        ret = '{},{},{},{},{},{}'.format(self.taizhang_id, self.shelf_id, self.type, self.length, self.height, self.depth)
         if len(self.data_levels)>0:
             ret += ':[\n'
             for data_level in self.data_levels:
                 ret += str(data_level)
                 ret += '\n'
-            ret += '\t]'
+            ret += ']'
         return ret
 
 class DataLevel():
@@ -211,13 +215,13 @@ class DataLevel():
         self.data_goods_array.append(data_goods)
 
     def __str__(self):
-        ret = '\t\t{},{},{}'.format(self.type, self.height, self.depth)
+        ret = '\t{},{},{}'.format(self.type, self.height, self.depth)
         if len(self.data_goods_array)>0:
             ret += ':[\n'
             for data_goods in self.data_goods_array:
                 ret += str(data_goods)
                 ret += '\n'
-            ret += '\t\t]'
+            ret += '\t]'
         return ret
 
 class DataGoods():
@@ -229,7 +233,7 @@ class DataGoods():
         self.depth = depth
 
     def __str__(self):
-        return '\t\t\t{},{},{},{},{}'.format(self.mch_code,self.upc,self.width,self.height,self.depth)
+        return '\t\t{},{},{},{},{}'.format(self.mch_code,self.upc,self.width,self.height,self.depth)
 
 class DataRawGoods():
     def __init__(self, mch_code, upc, corp_classify_code, spec, volume, width, height, depth, start_sum, multiple):
@@ -248,7 +252,7 @@ class DataRawGoods():
         return '{},{},{},{},{},{},{},{},{}'.format(self.mch_code,self.upc,self.corp_classify_code,self.spec,self.volume,self.width,self.height,self.depth,self.start_sum,self.multiple)
 
 if __name__ == "__main__":
-    data_shop = get_shop_shelfs(1284)
+    data_shop = get_raw_shop_shelfs(1284)
     print(data_shop)
 
     ret = get_raw_goods_info(1284,[2036329,2036330])
