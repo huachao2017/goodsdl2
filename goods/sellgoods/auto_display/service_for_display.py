c551cd695cc9541bc45db4b97662be6eef3213f9
TreesLi <wlgcxy2012@163.com>
2019-11-01 15:17:02 +0800

    陈列所需接口api  03

--- goods/sellgoods/auto_display/service_for_display.py
+++ goods/sellgoods/auto_display/service_for_display.py
@@ -39,40 +39,65 @@ def api_get_mark_goods(taizhang,shelf_id,kd_value=None):
 # 入参： shop_id
 # 入参： isfir 是否首次生成 陈列  如果是：使用好邻居的推荐销量  否：使用线上该店的真实销量
 # 返回： shop 对象  sellgoods 下 commonbean 下
-def caculate_goods(taizhang):
+def calculate_goods(taizhang):
     # TODO
     twidth_to_goods = {}
     mark = 0
-    for shelf in taizhang.shelfs:
-        for level in shelf.levels:
-            for good in level.goods:
-                good.display_num = max(good.sale_num*2,good.start_num)
-                one_face_goods_number = int(shelf.depth/good.depth)
-                if good.is_superimpose:   # 可叠放
-                    one_face_goods_number = one_face_goods_number*good.superimpose_rows
-                good.faces_num = max(good.display_num/one_face_goods_number,1)
+    shelf_depth = taizhang.shelfs[0].depth
+    for good in taizhang.calculate_goods_array:
+        good.display_num = max(good.sale_num*2,good.start_num)
+        one_face_goods_number = int(shelf_depth/good.depth)
+        if good.is_superimpose:   # 可叠放
+            one_face_goods_number = one_face_goods_number*good.superimpose_rows
+        good.faces_num = max(good.display_num/one_face_goods_number,1)
 
-                good.good_scale = good.faces_num*good.width
-                mark += good.good_scale
-                twidth_to_goods[mark] = good
+        good.good_scale = good.faces_num*good.width
+        mark += good.good_scale
+        twidth_to_goods[mark] = good
+        # twidth_to_goods[good.mch_good_code] = mark
     taizhang.twidth_to_goods = twidth_to_goods
+    taizhang.last_twidth = mark
 
 def update_mark_goods_array(taizhang,change_total_width):
+    """
+    更新货架所需的商品的刻度
+    :param taizhang:
+    :param change_total_width: 改变的宽度
+    :return:
+    """
 
-    if change_total_width < 0:
-        sum = 0
-        reversed_caculate_goods_array = list(reversed(taizhang.caculate_goods_array))
-        for good in reversed_caculate_goods_array:
-            a = math.fabs(change_total_width) - sum
-            sum += good.good_scale
-            b = sum - math.fabs(change_total_width)
-            if a > 0 and b > 0:
-                if a < b:
-                    pass
+    # if change_total_width < 0:
+    #     sum = 0
+    #     reversed_calculate_goods_array = list(reversed(taizhang.calculate_goods_array))
+    #     for good in reversed_calculate_goods_array:
+    #         a = math.fabs(change_total_width) - sum
+    #         sum += good.good_scale
+    #         b = sum - math.fabs(change_total_width)
+    #         if a > 0 and b > 0:
+    #             good_index = taizhang.calculate_goods_array.index(good)
+    #             if a > b:
+    #                 taizhang.last_twidth = taizhang.twidth_to_goods(taizhang.calculate_goods_array[good_index-1].mch_good_code)
+    #             else:
+    #                 taizhang.last_twidth = taizhang.twidth_to_goods(taizhang.calculate_goods_array[good_index].mch_good_code)
+    #
+    # if change_total_width > 0:
+    #     pass
+    tem = []
+    taizhang.last_twidth += change_total_width
+    for mark,good in taizhang.twidth_to_goods:
+        if mark < taizhang.last_twidth:
+            tem.append(good)
+    taizhang.calculate_goods_array = tem
 
-            if sum > change_total_width:
-                index = taizhang.caculate_goods_array.index(good)
-                taizhang.caculate_goods_array = 0
+def shelf_gap_choose_goods(taizhang,neighbour_good,level_goods,level_diff_width=None):
+    """
+    空隙补品
+    :param taizhang:
+    :param neighbour_good:
+    :param level_goods:
+    :param level_diff_width:
+    :return:
+    """
 
 
 
