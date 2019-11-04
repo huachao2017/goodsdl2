11be96e97187f72855594f1f5cdb70e6ff4f9eb0
huachao2017 <21006735@qq.com>
2019-11-04 18:53:10 +0800

    - 修复错误

--- goods/goodsdata.py
+++ goods/goodsdata.py
@@ -46,7 +46,7 @@ def get_raw_shop_shelfs(uc_shopid, tz_id = None):
         except:
             print('获取台账陈列失败：{}！'.format(taizhang_id))
             associated_catids = None
-        if associated_catids is None:
+        if associated_catids is None or associated_catids == '':
             associated_catids = '0501,0502,0503,0504,0505,0506'  # FIXME only for test
 
         cursor.execute("select t.shelf_no,s.length,s.height,s.depth from sf_shelf s, sf_shelf_type t where s.shelf_type_id=t.id and s.id={}".format(shelf_id))
