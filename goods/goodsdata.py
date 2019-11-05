6354a27b85753b8c64fa48bebb015160c1b3d13d
huachao2017 <21006735@qq.com>
2019-11-05 15:23:24 +0800

    - 增加订单数据获取

--- goods/goodsdata.py
+++ goods/goodsdata.py
@@ -129,6 +129,159 @@ def get_raw_goods_info(uc_shopid, mch_codes):
     cursor_erp.close()
     return ret
 
+def get_shop_order_goods(shopid, erp_shop_type=0):
+    """
+    获取商店的所有货架及货架上的商品信息，该方法在订货时用
+    :param shopid: fx系统的商店id
+    :return:返回一个DataRawGoods对象的map,key为mch_code
+    """
+
+    ret = {}
+    cursor = connections['ucenter'].cursor()
+    cursor_dmstore = connections['dmstore'].cursor()
+    cursor_erp = connections['erp'].cursor()
+    cursor_ai = connections['default'].cursor()
+
+    # 获取台账系统的uc_shopid
+    cursor.execute('select id, mch_id from uc_shop where mch_shop_code = {}'.format(shopid))
+    (uc_shopid, mch_id) = cursor.fetchone()
+
+    # 获取erp系统的erp_shopid
+    try:
+        cursor_dmstore.execute("select erp_shop_id from erp_shop_related where shop_id = {} and erp_shop_type = 0".format(shopid))
+        (erp_shop_id,) = cursor_dmstore.fetchone()
+
+
+        cursor_erp.execute("select authorized_shop_id from ms_relation WHERE is_authorized_shop_id={} and status=1".format(erp_shop_id))
+        (authorized_shop_id,) = cursor_erp.fetchone()
+
+        if erp_shop_type == 1:
+            try:
+                cursor_dmstore.execute(
+                    "select erp_shop_id from erp_shop_related where shop_id = {} and erp_shop_type = 1".format(shopid))
+                (erp_supply_shop_id,) = cursor_dmstore.fetchone()
+                if erp_supply_shop_id == authorized_shop_id:
+                    cursor_erp.execute(
+                        "select authorized_shop_id from ms_relation WHERE is_authorized_shop_id={} and status=1".format(
+                            erp_supply_shop_id))
+                    (authorized_shop_id,) = cursor_erp.fetchone()
+
+            except:
+                pass
+
+    except:
+        print('找不到供应商:{}！'.format(shopid))
+        authorized_shop_id = None
+
+    # 获取台账 TODO 只能获取店相关的台账，不能获取商家相关的台账
+    cursor.execute("select t.id, t.shelf_id, td.display_shelf_info, td.display_goods_info from sf_shop_taizhang st, sf_taizhang t, sf_taizhang_display td where st.taizhang_id=t.id and td.taizhang_id=t.id and td.status in (1,2) and td.approval_status=1 and st.shop_id = {}".format(uc_shopid))
+    taizhangs = cursor.fetchall()
+    for taizhang in taizhangs:
+        taizhang_id = taizhang[0]
+        shelf_id = taizhang[1]
+        display_shelf_info = taizhang[2]
+        display_goods_info = taizhang[3]
+        cursor.execute("select t.shelf_no,s.length,s.height,s.depth from sf_shelf s, sf_shelf_type t where s.shelf_type_id=t.id and s.id={}".format(shelf_id))
+        (shelf_no, shelf_length, shelf_height, shelf_depth) = cursor.fetchone()
+        display_shelf_info = json.loads(display_shelf_info)
+        display_goods_info = json.loads(display_goods_info)
+        shelfs = display_shelf_info['shelf']
+        shelf_dict = {}
+        goods_array_dict = {}
+        for shelf in shelfs:
+            shelf_dict[shelf['shelfId']] = shelf['layer']
+        for goods_info in display_goods_info:
+            goods_array_dict[goods_info['shelfId']] = goods_info['layerArray']
+
+        for shelfId in shelf_dict.keys():
+            level_array = shelf_dict[shelfId]
+            goods_array = goods_array_dict[shelfId]
+            for i in range(len(level_array)):
+                level = level_array[i]
+                goods_level_array = goods_array[i]
+                level_depth = round(float(level['depth']))
+                for goods in goods_level_array:
+                    mch_code = goods['mch_goods_code']
+                    if mch_code not in goods:
+                        # 获取商品属性
+                        try:
+                            cursor.execute(
+                                "select id, goods_name,upc, tz_display_img, spec, volume, width,height,depth,is_superimpose,is_suspension from uc_merchant_goods where mch_id = {} and mch_goods_code = {}".format(
+                                    mch_id, mch_code))
+                            # FIXME width,height暂时翻转
+                            # (goods_id, goods_name, upc, tz_display_img, spec, volume, width, height, depth,is_superimpose,is_suspension) = cursor.fetchone()
+                            (goods_id, goods_name, upc, tz_display_img, spec, volume, height, width, depth, is_superimpose,
+                             is_suspension) = cursor.fetchone()
+                        except:
+                            print('台账找不到商品，只能把这个删除剔除:{}！'.format(mch_code))
+                            continue
+
+                        # 获取分类码
+                        try:
+                            cursor_dmstore.execute(
+                                "select id,corp_classify_code from goods where upc = '{}' and corp_goods_id={}".format(upc,
+                                                                                                                    mch_code))
+                            (goods_id, corp_classify_code) = cursor_dmstore.fetchone()
+                        except:
+                            print('dmstore找不到商品:{}-{}！'.format(upc, mch_code))
+                            corp_classify_code = None
+                            goods_id = 0
+
+                        # 获取库存
+                        try:
+                            cursor_dmstore.execute(
+                                "select stock from shop_goods where goods_id = {} and shop_id={}".format(goods_id,
+                                                                                                         shopid))
+                            (stock,) = cursor_dmstore.fetchone()
+                        except:
+                            print('dmstore找不到商店商品:{}-{}！'.format(upc, goods_id))
+                            stock = 0
+                        if erp_shop_type == 1:
+                            # TODO 获取erp 二批的库存
+                            pass
+
+                        if authorized_shop_id is not None:
+                            try:
+                                # 获取起订量
+                                # "select start_sum,multiple from ms_sku_relation where ms_sku_relation.status=1 and sku_id in (select sku_id from ls_sku where model_id = '{0}' and ls_sku.prod_id in (select ls_prod.prod_id from ls_prod where ls_prod.shop_id = {1} ))"
+                                cursor_erp.execute(
+                                    "select s.sku_id prod_id from ls_prod as p, ls_sku as s where p.prod_id = s.prod_id and p.shop_id = {} and s.model_id = '{}'".format(
+                                        authorized_shop_id, upc))
+                                (sku_id,) = cursor_erp.fetchone()
+                                cursor_erp.execute(
+                                    "select start_sum,multiple from ms_sku_relation where ms_sku_relation.status=1 and sku_id = {}".format(
+                                        sku_id))
+                                (start_sum, multiple) = cursor_erp.fetchone()
+                            except:
+                                print('Erp找不到商品:{}-{}！'.format(upc, mch_code))
+                                start_sum = 0
+                                multiple = 0
+                        else:
+                            start_sum = 0
+                            multiple = 0
+
+                        # 获取预测销量
+                        # TODO
+
+                        cursor.execute(
+                            "select id, upc, spec, volume, width,height,depth from uc_merchant_goods where mch_id = {} and mch_goods_code = {}".format(
+                                mch_id, goods['mch_goods_code']))
+                        (goods_id, upc, spec, volume, width, height, depth) = cursor.fetchone()
+                        ret[mch_code] = DataRawGoods(mch_code, goods_name, upc, tz_display_img,corp_classify_code, spec, volume, width, height, depth,
+                                                     is_superimpose,is_suspension, start_sum,multiple,
+                                                     stock = stock,
+                                                     sales = 0,
+                                                     shelf_depth=level_depth)
+
+    cursor.close()
+    cursor_dmstore.close()
+    cursor_erp.close()
+    cursor_ai.close()
+
+    return ret
+
+
+
 def get_shop_shelf_goods(shopid):
     """
     获取商店的所有货架及货架上的商品信息，该方法在订货时用
@@ -259,7 +412,7 @@ class DataGoods():
         return '\t\t{},{},{},{},{}'.format(self.mch_code,self.upc,self.width,self.height,self.depth)
 
 class DataRawGoods():
-    def __init__(self, mch_code, goods_name, upc, tz_display_img,corp_classify_code, spec, volume, width, height, depth, is_superimpose,is_suspension, start_sum, multiple):
+    def __init__(self, mch_code, goods_name, upc, tz_display_img, corp_classify_code, spec, volume, width, height, depth, is_superimpose, is_suspension, start_sum, multiple, stock=0, sales=0, shelf_depth=0):
         self.mch_code = mch_code
         self.goods_name = goods_name
         self.upc = upc
@@ -281,9 +434,15 @@ class DataRawGoods():
             self.is_suspension = False
         self.start_sum = start_sum
         self.multiple = multiple
+        self.stock = stock
+        self.sales = sales
+        self.shelf_depth = shelf_depth
 
     def __str__(self):
-        return '{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}'.format(self.mch_code,self.goods_name,self.upc,self.tz_display_img,self.corp_classify_code,self.display_code,self.spec,self.volume,self.width,self.height,self.depth,self.is_superimpose,self.is_suspension,self.start_sum,self.multiple)
+        return '{},{},{},{},{},{},{},{},' \
+               '{},{},{},{},{},{},{},{},{},{}'.format(
+            self.mch_code,self.goods_name,self.upc,self.tz_display_img,self.corp_classify_code,self.display_code,self.spec,self.volume,
+            self.width,self.height,self.depth,self.is_superimpose,self.is_suspension,self.start_sum,self.multiple,self.stock,self.sales,self.shelf_depth)
 
 if __name__ == "__main__":
     ret = get_raw_shop_shelfs(806)
@@ -294,7 +453,5 @@ if __name__ == "__main__":
     ret = get_raw_goods_info(806,[2036329,2036330])
     print("\n".join('{}:{}'.format(str(i),str(ret[i])) for i in ret.keys()))
 
-    # ret = get_shop_shelf_goods(1284)
-    # for data_shelf in ret:
-    #     print(str(data_shelf))
-    #     print('\n')
+    ret_goods = get_shop_order_goods(1284,0)
+    print("\n".join('{}:{}'.format(str(i),str(ret_goods[i])) for i in ret_goods.keys()))
