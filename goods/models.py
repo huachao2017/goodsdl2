7e17705c923807572221569cdad5adfebe6f22cd
huachao2017 <21006735@qq.com>
2019-11-05 13:46:27 +0800

    - 增加两个字段

--- goods/models.py
+++ goods/models.py
@@ -102,6 +102,8 @@ class ai_sales_goods(models.Model):
 
 class ai_sales_order(models.Model):
     shopid = models.IntegerField()
+    erp_shop_type = models.IntegerField()
+    status = models.IntegerField(default=1) #0为待订货，1为已订货
     upc = models.CharField(max_length=20)
     order_sale = models.IntegerField() #订货量
     predict_sale = models.IntegerField() #预测销量
