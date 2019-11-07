33b56838318fd860c12dd1b2fe32afb8081d29cc
TreesLi <wlgcxy2012@163.com>
2019-11-07 13:57:15 +0800

    日配选品存库

--- goods/sellgoods/auto_choose_goods/haolinju_template.py
+++ goods/sellgoods/auto_choose_goods/haolinju_template.py
@@ -74,7 +74,7 @@ def get_data(target,template_shop_id,days=28):
 def storage_day_choose(data):
     # 以下查询保质期的长短
     upcs = [str(i[2]) for i in data]
-    sql_02 = 'select upc from ucenter.uc_merchant_goods where upc in {} and storage_day>11'
+    sql_02 = 'select upc from ucenter.uc_merchant_goods where upc in {} and storage_day=0'
     cursor_02 = connections['ucenter'].cursor()
     # print(tuple(upcs))
     cursor_02.execute(sql_02.format(tuple(upcs)))
@@ -252,7 +252,7 @@ if __name__ == '__main__':
 
     a = get_data(1284,'3598')
     # print(a)
-    # a = storage_day_choose(a)
+    a = storage_day_choose(a)
     b = choose_goods(a)
     # print(b)
     # print(len(a))
