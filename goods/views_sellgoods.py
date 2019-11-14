b5d06c98a74257b7eed4c19eb4267914b93107a2
LRJliurj <908601417@qq.com>
2019-11-14 17:35:39 +0800

    添加日配和非日配区分逻辑

--- goods/views_sellgoods.py
+++ goods/views_sellgoods.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from goods.sellgoods.salesquantity.service import generate_order_shop
-
+from concurrent.futures import ThreadPoolExecutor
 logger = logging.getLogger("django")
 class Test(APIView):
     def get(self, request):
@@ -15,7 +15,8 @@ class Test(APIView):
 class SellGoodsViewSet(APIView):
     def get(self,request):
         shop_id = request.query_params['shop_id']
-        generate_order_shop.generate(shopid = shop_id)
+        with ThreadPoolExecutor(max_workers=5) as t:  # 创建一个最大容纳数量为5的线程池
+            t.submit(generate_order_shop.generate, shopid=shop_id)
         print ("shop_id=%s, notify_shop_order_generate success..."% str(shop_id))
         return Response(status=status.HTTP_200_OK)
 
