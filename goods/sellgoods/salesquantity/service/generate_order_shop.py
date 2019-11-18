93d12ed533f117ed624b94648e2c785a0dd7f033
LRJliurj <908601417@qq.com>
2019-11-18 11:20:14 +0800

    dd

--- goods/sellgoods/salesquantity/service/generate_order_shop.py
+++ goods/sellgoods/salesquantity/service/generate_order_shop.py
@@ -10,11 +10,9 @@ shop_type = config.shellgoods_params['shop_types'][0]  # 门店
 order_shop_hours = config.shellgoods_params['order_shop_hours']
 yinliao_cat_ids = config.shellgoods_params['yinliao_cat_ids'] # 饮料台账分类
 def generate(shopid1 = None):
-    # order_shop_ids1 = None
-    # if shopid1 is not None:
-    #     order_shop_ids1 = [shopid1]
-    # if order_shop_ids1 is not None:
-    #     order_shop_ids = order_shop_ids1
+    order_shop_ids = config.shellgoods_params['order_shop_hour_ids']
+    if shopid1 is not None:
+        order_shop_ids = [shopid1]
     # TODO 订货量 由库存计算改为用实际销售量计算
     for shop_id in order_shop_ids:
         result = get_shop_order_goods(shop_id,shop_type)
@@ -35,7 +33,7 @@ def generate(shopid1 = None):
             if sales_order_ins.order_sale > 0:
                 sales_order_inss.append(sales_order_ins)
         if len(sales_order_inss) > 0:
-            erp_interface.order_commit(shop_id, shop_type, sales_order_inss)
+            # erp_interface.order_commit(shop_id, shop_type, sales_order_inss)
             print("erp_interface.order_commit success!")
 
 if __name__=='__main__':
