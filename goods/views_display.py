808995e232009563d68324770437b180252e82c8
huachao2017 <21006735@qq.com>
2019-11-21 18:20:16 +0800

    - fix

--- goods/views_display.py
+++ goods/views_display.py
@@ -2,6 +2,7 @@ import json
 import logging
 from rest_framework.response import Response
 from rest_framework import mixins
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework import status
 from goods.shelfdisplay.generate_shelf_dispaly import generate_displays
@@ -12,7 +13,8 @@ from .serializers import *
 
 logger = logging.getLogger("django")
 
-class ShelfDisplayDebugViewSet(mixins.ListModelMixin):
+class ShelfDisplayDebugViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
+                               viewsets.GenericViewSet):
     queryset = ShelfDisplayDebug.objects.order_by('-id')
     serializer_class = ShelfDisplayDebugSerializer
 
