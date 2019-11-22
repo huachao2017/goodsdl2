862370f2ae4f032776f992e20be2640f06a59846
TreesLi <wlgcxy2012@163.com>
2019-11-22 18:11:43 +0800

    -test

--- goods/shelfdisplay/generate_shelf_dispaly.py
+++ goods/shelfdisplay/generate_shelf_dispaly.py
@@ -104,4 +104,4 @@ def print_taizhang(taizhang, image_dir):
 
 if __name__ == "__main__":
     # taizhang = generate_displays(806, 1187)
-    taizhang = generate_displays(806, 1199)
+    taizhang = generate_displays(806, 1211)
