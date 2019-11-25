eec90627666d66775543c8120ba0f9d9910ace6b
LRJliurj <908601417@qq.com>
2019-11-25 10:53:27 +0800

    添加约束自动订货

--- goods/sellgoods/salesquantity/service/order_version_3/generate_order_2saler_add_day.py
+++ goods/sellgoods/salesquantity/service/order_version_3/generate_order_2saler_add_day.py
@@ -20,11 +20,12 @@ def generate(shop_id = None):
             if drg_ins.isnew_goods:
                 order_sale = drg_ins.max_disnums
             else:
-                if drg_ins.safe_day_nums*drg_ins.old_sales/7 - drg_ins.stock - drg_ins.supply_stock >0:
-                # print("规则1 ：max(安全天数内的销量，最小陈列量，起订量)")
-                    order_sale = max(drg_ins.safe_day_nums*drg_ins.old_sales/7,drg_ins.min_disnums,drg_ins.start_sum) - drg_ins.stock
+                if drg_ins.safe_day_nums * drg_ins.old_sales / 7 - drg_ins.stock >= 0:
+                    # print("规则1 ：max(安全天数内的销量，最小陈列量，起订量)")
+                    order_sale = max(drg_ins.safe_day_nums * drg_ins.old_sales / 7, drg_ins.min_disnums,
+                                     drg_ins.start_sum) - drg_ins.stock
                 else:
-                    order_sale = 0 
+                    order_sale = 0
             if order_sale <= 0:
                 continue
             # print("规则2： 起订量规则")
