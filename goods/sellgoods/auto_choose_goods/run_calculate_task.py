69af4c27a0abd888d6e06c1862eb2b00e3aa6b29
TreesLi <wlgcxy2012@163.com>
2019-11-28 19:50:24 +0800

    test

--- goods/sellgoods/auto_choose_goods/run_calculate_task.py
+++ goods/sellgoods/auto_choose_goods/run_calculate_task.py
@@ -25,27 +25,28 @@ if __name__ == '__main__':
         if all_data:
             for data in all_data:
                 start_time = time.time()
-                # try:
-                cursor.execute(update_sql_01.format(2,data[0]))   # 更新到“正在计算”
-                print('正在计算中...')
-                conn.commit()
-                # conn.close()
-                ucenter_conn = connections['ucenter']
-                ucenter_cursor = ucenter_conn.cursor()
-                ucenter_cursor.execute(select_sql_shopid.format(data[2]))
-                shopid = ucenter_cursor.fetchone()[0]
-                ucenter_conn.close()
+                try:
+                    cursor.execute(update_sql_01.format(2,data[0]))   # 更新到“正在计算”
+                    print('正在计算中...')
+                    conn.commit()
+                    # conn.close()
+                    ucenter_conn = connections['ucenter']
+                    ucenter_cursor = ucenter_conn.cursor()
+                    ucenter_cursor.execute(select_sql_shopid.format(data[2]))
+                    shopid = ucenter_cursor.fetchone()[0]
+                    ucenter_conn.close()
 
-                # time.sleep(5)
-                # for i in range(3):
-                print('shopid',shopid)
-                print('id',data[0])
+                    # time.sleep(5)
+                    # for i in range(3):
+                    print('shopid',shopid)
+                    print('id',data[0])
 
-                start_choose_goods(data[1],shopid,conn)   #计算中
+                    start_choose_goods(data[1],shopid,conn)   #计算中
 
-                # except:
-                #     cursor.execute(update_sql_01.format(4, data[0]))  # 4代表失败
-                #     continue
+                except Exception as e:
+                    print('Exception',e)
+                    cursor.execute(update_sql_01.format(4, data[0]))  # 4代表失败
+                    continue
 
                 cursor = conn.cursor()
                 cursor.execute(update_sql_02.format(math.ceil(time.time() - start_time),data[0]))  # 更新到“结束计算”和耗时多少
