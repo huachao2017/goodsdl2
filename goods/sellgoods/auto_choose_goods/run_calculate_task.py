5e938ff3e8daaf33ed89f07d93a152c3a429d9e2
TreesLi <wlgcxy2012@163.com>
2019-11-28 14:11:08 +0800

    错误修复

--- goods/sellgoods/auto_choose_goods/run_calculate_task.py
+++ goods/sellgoods/auto_choose_goods/run_calculate_task.py
@@ -1,7 +1,7 @@
 
 import pymysql
 import datetime
-import os,django,time
+import os,django,time,math
 from goods.sellgoods.auto_choose_goods.haolinju_template import start_choose_goods
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     # conn = pymysql.connect('10.19.68.63', 'gpu_rw', password='jyrMnQR1NdAKwgT4', database='goodsdl',charset="utf8", port=3306, use_unicode=True)
     # cursor = conn.cursor()
     select_sql = "select id,batch_id,uc_shopid from goods_allworkflowbatch where select_goods_status=1"
+    select_sql_shopid = "select mch_shop_code from uc_shop where id = {}"
     update_sql_01 = "update goods_allworkflowbatch set select_goods_status=2 where id={}"    #2是正在计算、3是计算结束
     update_sql_02 = "update goods_allworkflowbatch set select_goods_status=3,select_goods_calculate_time={} where id={}"    #2是正在计算、3是计算结束
     while True:
@@ -24,13 +25,18 @@ if __name__ == '__main__':
         if all_data:
             for data in all_data:
                 cursor.execute(update_sql_01.format(data[0]))   # 更新到“正在计算”
-                print('正在计算中')
+                print('正在计算中...')
                 conn.commit()
                 # conn.close()
+                ucenter_conn = connections['ucenter'].cursor()
+                ucenter_cursor = ucenter_conn.cursor()
+                ucenter_cursor.execute(select_sql_shopid.format(data[2]))
+                shopid = cursor.fetchone()[0]
+                ucenter_conn.close()
                 start_time = time.time()
                 # time.sleep(5)
-                start_choose_goods(data[1],data[2],conn)   #计算中
+                start_choose_goods(data[1],shopid,conn)   #计算中
                 cursor = conn.cursor()
-                cursor.execute(update_sql_02.format(int(time.time() - start_time),data[0]))  # 更新到“结束计算”和耗时多少
+                cursor.execute(update_sql_02.format(math.ceil(time.time() - start_time),data[0]))  # 更新到“结束计算”和耗时多少
                 conn.commit()
         conn.close()
