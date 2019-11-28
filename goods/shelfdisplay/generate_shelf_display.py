ad4c073fd44ba453ec5537cd8f03c7454c926dd8
huachao2017 <21006735@qq.com>
2019-11-28 13:29:44 +0800

    - 陈列完不通知台账

--- goods/shelfdisplay/generate_shelf_display.py
+++ goods/shelfdisplay/generate_shelf_display.py
@@ -40,16 +40,16 @@ def generate_workflow_displays(uc_shopid, batch_id):
     auto_display_calculate_time = int(end_time-begin_time)
 
     # 通知台账系统
-    url = "https://autodisplay:xianlife2018@taizhang.aicvs.cn/api/autoDisplay"
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-    for taizhang_display in taizhang_display_list:
-        json_info = json.dumps(taizhang_display.to_json())
-        data = bytes(json_info, 'utf8')
-        resp = requests.post(url=url, data=data, headers=headers)
-        print('通知台账系统：'.format(resp))
+    # url = "https://autodisplay:xianlife2018@taizhang.aicvs.cn/api/autoDisplay"
+    # headers = {
+    #     "Accept": "application/json",
+    #     "Content-Type": "application/json"
+    # }
+    # for taizhang_display in taizhang_display_list:
+    #     json_info = json.dumps(taizhang_display.to_json())
+    #     data = bytes(json_info, 'utf8')
+    #     resp = requests.post(url=url, data=data, headers=headers)
+    #     print('通知台账系统：'.format(resp))
 
     # 更新workflow
     workflow.auto_display_status = 3
