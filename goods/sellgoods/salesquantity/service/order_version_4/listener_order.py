749fbcb4afa27d250e955252811f18551cbb4b47
LRJliurj <908601417@qq.com>
2019-11-29 19:30:57 +0800

    dd

--- goods/sellgoods/salesquantity/service/order_version_4/listener_order.py
+++ goods/sellgoods/salesquantity/service/order_version_4/listener_order.py
@@ -6,24 +6,27 @@ import traceback
 def generate_data():
     print ("生成自动订单数据")
     while True:
-        time.sleep(5)
+        time.sleep(30)
         close_old_connections()
         try:
             order_process.first_order_process()
         except Exception as e:
             print ("order_process.first_order_process  error e={}".format(e))
+            time.sleep(20)
             traceback.print_exc()
 
         try:
             order_process.day_order_process()
         except Exception as e:
             print("order_process.day_order_process  error e={}".format(e))
+            time.sleep(20)
             traceback.print_exc()
 
         try:
             order_process.add_order_process()
         except Exception as e:
             print("order_process.add_order_process  error e={}".format(e))
+            time.sleep(20)
             traceback.print_exc()
 if __name__=='__main__':
     generate_data()
