de0b8c58e32ed712f5bae1ba114f13d6fb48c105
huachao2017 <21006735@qq.com>
2019-12-02 10:56:12 +0800

    - 防止日志的io问题

--- goods/shelfdisplay/workflow_deamon_process.py
+++ goods/shelfdisplay/workflow_deamon_process.py
@@ -9,22 +9,20 @@ from goods.shelfdisplay.generate_shelf_display import generate_workflow_displays
 from goods.models import AllWorkFlowBatch
 
 
-class Logger(object):
-    def __init__(self, filename="Default.log"):
-        self.terminal = sys.stdout
-        self.log = open(filename, "a")
-
-    def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)
-
-    def flush(self):
-        pass
+# class Logger(object):
+#     def __init__(self, filename="Default.log"):
+#         self.terminal = sys.stdout
+#         self.log = open(filename, "a")
+#
+#     def write(self, message):
+#         self.terminal.write(message)
+#         self.log.write(message)
+#
+#     def flush(self):
+#         pass
 
 if __name__ == "__main__":
-    path = os.path.abspath(os.path.dirname(__file__))
-    type = sys.getfilesystemencoding()
-    sys.stdout = Logger('/home/src/goodsdl2/logs/display.log')
+    # sys.stdout = Logger('/home/src/goodsdl2/logs/display.log')
     while True:
         print('workflow deamon is alive')
 
@@ -50,4 +48,4 @@ if __name__ == "__main__":
         except Exception as e:
             print('守护进程出现错误：{}'.format(e))
 
-        time.sleep(60)
+        time.sleep(10)
