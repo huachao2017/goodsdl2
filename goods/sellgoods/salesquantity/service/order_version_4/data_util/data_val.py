3d48972cbbb601cd6ee1deb7083c648785027c24
LRJliurj <908601417@qq.com>
2019-12-04 12:38:53 +0800

    加入在途 和 起订价

--- goods/sellgoods/salesquantity/service/order_version_4/data_util/data_val.py
+++ goods/sellgoods/salesquantity/service/order_version_4/data_util/data_val.py
@@ -1,6 +1,6 @@
 from set_config import config
 from goods.sellgoods.salesquantity.service.order_version_4.data_util import cacul_util
-shop_type = config.shellgoods_params['shop_types'][0]  # 门店
+shop_type = config.shellgoods_params['shop_types'][1]  # 门店
 
 def generate(shop_id = None):
     print ("数据验证程序,shop_id"+str(shop_id))
