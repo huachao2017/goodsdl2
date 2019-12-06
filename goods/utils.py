0dc95882560ecfa8203510ce7aa889959921f43e
TreesLi <wlgcxy2012@163.com>
2019-12-06 10:38:41 +0800

    数据异常报警

--- goods/utils.py
+++ goods/utils.py
@@ -10,6 +10,16 @@ import time
 import datetime
 from  decimal import Decimal
 
+import datetime
+import main.import_django_settings
+import math
+from django.db import connections
+import traceback
+from set_config import config
+from goods.third_tools.dingtalk import send_message
+from goods.sellgoods.salesquantity.service.order_version_5.data_util.goods_info import *
+
+
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 # django.setup()
 
@@ -250,6 +260,221 @@ def check_order():
         print()
         print()
 
+def data_exception_alarm(shopid):
+    """
+        获取商店的所有货架及货架上的商品信息，进行数据异常报警
+        :param shopid: fx系统的商店id
+        :param erp_shop_type: erp系统里面的类型
+        :return:返回一个DataRawGoods对象的map,key为mch_code
+        """
+    ret = {}
+    next_day = str(time.strftime('%Y-%m-%d', time.localtime()))
+    cursor = connections['ucenter'].cursor()
+    cursor_dmstore = connections['dmstore'].cursor()
+    cursor_erp = connections['erp'].cursor()
+    cursor_ai = connections['default'].cursor()
+    try:
+        cursor_bi = connections['bi'].cursor()
+    except:
+        cursor_bi = None
+    # 获取台账系统的uc_shopid
+    cursor.execute('select id, shop_name , mch_id from uc_shop where mch_shop_code = {}'.format(shopid))
+    (uc_shopid, shop_name, mch_id) = cursor.fetchone()
+    if not uc_shopid:
+        send_message('{}在台账系统找不到对应的shopid！'.format(shopid))
+    # 获取erp系统的erp_shopid
+    cursor_dmstore.execute(
+        "select erp_shop_id from erp_shop_related where shop_id = {} and erp_shop_type = 0".format(shopid))
+    (erp_shop_id,) = cursor_dmstore.fetchone()  # 门店id
+    cursor_dmstore.execute(
+        "select erp_shop_id from erp_shop_related where shop_id = {} and erp_shop_type = 1".format(shopid))
+    (erp_supply_id,) = cursor_dmstore.fetchone()  # 仓库id
+
+    cursor_dmstore.execute(
+        "select erp_shop_id from erp_shop_related where shop_id = {} and erp_shop_type = 2".format(shopid))
+    (erp_resupply_id,) = cursor_dmstore.fetchone()  # 供货商id
+
+    # 获取台账 TODO 只能获取店相关的台账，不能获取商家相关的台账
+    cursor.execute(
+        "select t.id, t.shelf_id, td.display_shelf_info, td.display_goods_info from sf_shop_taizhang st, sf_taizhang t, sf_taizhang_display td where st.taizhang_id=t.id and td.taizhang_id=t.id and td.status in (1,2) and td.approval_status=1 and st.shop_id = {}".format(
+            uc_shopid))
+    taizhangs = cursor.fetchall()
+    shelf_inss = []
+    for taizhang in taizhangs:
+        taizhang_id = taizhang[0]
+        shelf_id = taizhang[1]
+        shelf_type = ''
+        shelf_type_id = None
+        try:
+            cursor.execute("select id,shelf_type_id from sf_shelf where id = {}".format(shelf_id))
+            (id, shelf_type_id) = cursor.fetchone()
+        except:
+            print("台账找不到货架 ， shelf_id=" + str(shelf_id))
+            traceback.print_exc()
+
+        try:
+            cursor.execute("select id,type_name from sf_shelf_type where id = {} ".format(shelf_type_id))
+            (id, type_name) = cursor.fetchone()
+            shelf_type = type_name
+        except:
+            print("台账找不到货架类型名称 ， shelf_type_id=" + str(shelf_type_id))
+            traceback.print_exc()
+        display_shelf_info = taizhang[2]
+        display_goods_info = taizhang[3]
+        display_shelf_info = json.loads(display_shelf_info)
+        display_goods_info = json.loads(display_goods_info)
+        shelfs = display_shelf_info['shelf']
+        shelf_dict = {}
+        goods_array_dict = {}
+        for shelf in shelfs:
+            shelf_dict[shelf['shelfId']] = shelf['layer']
+        for goods_info in display_goods_info:
+            goods_array_dict[goods_info['shelfId']] = goods_info['layerArray']
+
+        for shelfId in shelf_dict.keys():
+            level_array = shelf_dict[shelfId]
+            goods_array = goods_array_dict[shelfId]
+            for i in range(len(level_array)):
+                level = level_array[i]
+                goods_level_array = goods_array[i]
+                level_depth = round(float(level['depth']))
+                if level_depth is None or level_depth <= 0:
+                    send_message('id为{}的货架的第{}层深度为{}'.format(shelfId,i+1,level_depth))
+                for goods in goods_level_array:
+                    mch_code = goods['mch_goods_code']
+                    shelf_ins = Shelf()
+                    shelf_ins.taizhang_id = taizhang_id
+                    shelf_ins.shelf_id = shelf_id
+                    shelf_ins.shelf_type = shelf_type
+                    shelf_ins.mch_code = mch_code
+                    shelf_ins.goods_level_id = i
+                    shelf_ins.level_depth = level_depth
+                    shelf_ins.face_num = 1
+                    shelf_inss.append(shelf_ins)
+                    if mch_code in ret:
+                        print("该商品已加入")
+                    else:
+                        # 获取商品属性
+                        try:
+                            cursor.execute(
+                                "select id, goods_name,upc, tz_display_img, spec, volume, width,height,depth,is_superimpose,is_suspension,delivery_type,category1_id,category2_id,category_id,storage_day,package_type from uc_merchant_goods where mch_id = {} and mch_goods_code = {}".format(
+                                    mch_id, mch_code))
+                            # FIXME width,height暂时翻转
+                            # (goods_id, goods_name, upc, tz_display_img, spec, volume, width, height, depth,is_superimpose,is_suspension) = cursor.fetchone()
+                            (goods_id, goods_name, upc, tz_display_img, spec, volume, height, width, depth,
+                             is_superimpose,
+                             is_suspension, delivery_type, category1_id, category2_id, category_id, storage_day,
+                             package_type) = cursor.fetchone()
+
+                            if upc is None or category_id is None or storage_day is None or depth is None or delivery_type is None:
+                                send_message('mch_code为{}的商品存在空字段，upc:{}，三级分类(category_id):{}，保质期(storage_day):{}，深(depth):{}，配送方式(delivery_type):{}'.format(mch_code,upc,category_id,storage_day,depth,delivery_type))
+
+
+
+                        except:
+                            send_message('mch_code为{}的商品在库里找不到'.format(mch_code))
+
+                            print('台账找不到商品，只能把这个删除剔除:{}！'.format(mch_code))
+                            continue
+                        scale = None
+                        # 获取最大陈列系数
+
+
+
+
+                        # 单价和库存
+                        try:
+                            cursor_dmstore.execute(
+                                "select id,price,purchase_price,stock FROM shop_goods where upc = '{}' and shop_id = {} order by modify_time desc ".format(
+                                    upc, shopid))
+                            (id, upc_price, purchase_price, stock) = cursor_dmstore.fetchone()
+                            if upc_price is None or upc_price <= 0:
+                                send_message('{}(uc店号:{},mch_code:{},upc:{})—>>商品单价异常:{}'.format(goods_name,uc_shopid,mch_code,upc,upc_price))
+                            if stock is None or stock < 0:
+                                send_message('{}(uc店号:{},mch_code:{},upc:{})—>>商品库存异常:{}'.format(goods_name,uc_shopid,mch_code,upc,stock))
+
+                        except:
+                            send_message('{}(uc店号:{},mch_code:{},upc:{})—>>商品库存和单价获取失败'.format(goods_name, uc_shopid, mch_code,upc))
+                            continue
+
+
+
+                        if erp_resupply_id is not None:
+                            try:
+                                # 获取起订量
+                                # "select start_sum,multiple from ms_sku_relation where ms_sku_relation.status=1 and sku_id in (select sku_id from ls_sku where model_id = '{0}' and ls_sku.prod_id in (select ls_prod.prod_id from ls_prod where ls_prod.shop_id = {1} ))"
+                                cursor_erp.execute(
+                                    "select s.sku_id prod_id from ls_prod as p, ls_sku as s where p.prod_id = s.prod_id and p.shop_id = {} and s.model_id = '{}'".format(
+                                        erp_resupply_id, upc))
+                                (sku_id,) = cursor_erp.fetchone()
+                                cursor_erp.execute(
+                                    "select start_sum,multiple from ms_sku_relation where ms_sku_relation.status=1 and sku_id = {}".format(
+                                        sku_id))
+                                (start_sum, multiple) = cursor_erp.fetchone()
+
+                                if not start_sum:
+                                    send_message('upc为{}的商品起订量为{}'.format(upc,start_sum))
+                                else:
+                                    if start_sum <= 0:
+                                        send_message('upc为{}的商品起订量为{}'.format(upc, start_sum))
+
+
+                            except:
+                                send_message('upc为{}的商品起订量获取失败'.format(upc))
+
+                                print('Erp找不到商品:{}-{}！'.format(upc, erp_resupply_id))
+                                start_sum = 0
+                                multiple = 0
+                        else:
+                            start_sum = 0
+                            multiple = 0
+
+                        try:
+                            # 获取小仓库库存
+                            cursor_erp.execute(
+                                "select s.sku_id prod_id from ls_prod as p, ls_sku as s where p.prod_id = s.prod_id and p.shop_id = {} and s.model_id = '{}'".format(
+                                    erp_supply_id, upc))
+                            (sku_id,) = cursor_erp.fetchone()
+                            cursor_erp.execute(
+                                "select stock from ms_sku_relation where ms_sku_relation.status=1 and sku_id = {}".format(
+                                    sku_id))
+                            (supply_stock,) = cursor_erp.fetchone()
+                            if supply_stock is None or supply_stock < 0:
+                                send_message('upc为{}的商品小仓库库存为{}'.format(upc,supply_stock))
+                        except:
+                            send_message('upc为{}的商品小仓库库存获取失败'.format(upc))
+
+                            print('ErpSupply找不到商品:{}-{}！'.format(upc, erp_supply_id))
+                            supply_stock = 0
+
+
+                        # 获取商品的上架时间、是否新品
+                        try:
+                            cursor_ai.execute(
+                                "select up_shelf_date,is_new_goods from goods_up_shelf_datetime where shopid={} and upc='{}'".format(
+                                    uc_shopid, upc))
+                            (up_shelf_date, up_status) = cursor_ai.fetchone()
+                            print('ai找到商品上架时间 :{}-{}！'.format(uc_shopid, upc))
+                            if not up_status in (0,1):
+                                send_message('name为{}、upc为{}的商品的"是否新品"字段异常，is_new_goods：{}'.format(goods_name,upc,up_status))
+                        except:
+                            # print('ai找不到销量预测:{}-{}-{}！'.format(shopid,upc,next_day))
+                            up_shelf_date = str(time.strftime('%Y-%m-%d', time.localtime()))
+                            up_status = 1
+
+
+
+
+
+    cursor.close()
+    cursor_dmstore.close()
+    cursor_erp.close()
+    cursor_ai.close()
+    if cursor_bi is not None:
+        cursor_bi.close()
+    return ret
+
+
 if __name__ == '__main__':
     # calculate_goods_up_datetime(806)
 
@@ -257,4 +482,6 @@ if __name__ == '__main__':
 
     # print(select_psd_data('6921581540102',1284,28))
 
-    check_order()
+    # check_order()
+
+    data_exception_alarm(1284)
