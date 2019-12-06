91d7f99958cd017adbf730db24ae3731a8880658
huachao2017 <21006735@qq.com>
2019-12-06 15:15:34 +0800

    - 增加延时控制

--- goods/third_tools/dingtalk.py
+++ goods/third_tools/dingtalk.py
@@ -2,6 +2,11 @@ import json
 import requests
 import main.import_django_settings
 from django.conf import settings
+import time
+
+list_total = 20
+send_id_list = []
+send_id_to_time = {}
 
 def send_message(msg, type = 0):
     """
@@ -10,6 +15,38 @@ def send_message(msg, type = 0):
     :param type: 0其他业务报警，1是选品，2是陈列，3是订货
     :return:
     """
+    if len(send_id_list) == list_total:
+        # 判断发送队列数据是否要sleep
+        cur_time = time.time()
+        first_time = send_id_to_time[send_id_list[0]]
+        if cur_time - first_time < 60:
+            sleep_time = 60-int(cur_time-first_time)
+            print('发送过于频繁，sleep {}秒'.format(sleep_time))
+            time.sleep(sleep_time)
+
+        # 清理列表
+        cur_time = time.time()
+        remove_list = []
+        for send_id in send_id_list:
+            if cur_time - send_id_to_time[send_id] > 60:
+                remove_list.append(send_id)
+            else:
+                break
+
+        for send_id in remove_list:
+            send_id_list.remove(send_id)
+            send_id_to_time.pop(send_id)
+
+
+    # 增加发送队列数据
+    if len(send_id_list) == 0:
+        send_id = 0
+    else:
+        send_id = send_id_list[-1]+1
+
+    send_id_list.append(send_id)
+    send_id_to_time[send_id] = time.time()
+
     is_test_server = settings.IS_TEST_SERVER
     if is_test_server:
         access_token = '5200bc97f99cf1b46fdefa32e5334e45dcc92080b9276a39c3a8832b0675a840'
@@ -44,4 +81,5 @@ def send_message(msg, type = 0):
 
 
 if __name__ == "__main__":
-    send_message('测试信息！')
+    for i in range(50):
+        send_message('测试信息{}！'.format(i))
