3792592ba5334facbde11bea4a12cf96ebf81869
LRJliurj <908601417@qq.com>
2019-12-09 19:28:59 +0800

    oversion5

--- goods/sellgoods/salesquantity/service/order_version_5/order_process.py
+++ goods/sellgoods/salesquantity/service/order_version_5/order_process.py
@@ -13,9 +13,9 @@ import traceback
 sql_workflow = "select id,batch_id,uc_shopid from goods_allworkflowbatch where type = {} and order_goods_status=1"
 update_sql_01 = "update goods_allworkflowbatch set order_goods_status=2 where id={}"  # 2是正在计算、3是计算结束
 update_sql_02 = "update goods_allworkflowbatch set order_goods_status={},order_goods_calculate_time={} where id={}"  # 2是正在计算、3是计算结束
-insert_goods_batch_order = "insert into goods_batch_order (batch_order_id,order_data,create_time,update_time,uc_shop_id) values (%s,%s,%s,%s,%s)"
+insert_goods_batch_order = "insert into goods_batch_order (batch_order_id,order_data,create_time,update_time,uc_shop_id,order_all_data) values (%s,%s,%s,%s,%s,%s)"
 select_goods_batch_order = "select id,batch_order_id order_data from goods_batch_order where batch_order_id='{}' and uc_shop_id = {} "
-update_goods_batch_order = "update goods_batch_order set order_data = '{}',update_time='{}' where id = {}"
+update_goods_batch_order = "update goods_batch_order set order_data = '{}',update_time='{}',order_all_data='{}' where id = {}"
 sql_uc_shop = "select mch_shop_code from uc_shop where id = {}"
 # 首次订货单
 def first_order_process():
@@ -38,7 +38,7 @@ def first_order_process():
                 cursor_ai.connection.commit()
                 start_time = time.time()
                 print("首次订单 batch_id =" + str(batch_id))
-                sales_order_inss = generate_order_2saler_first.generate(dmstore_shopid)
+                sales_order_inss,result = generate_order_2saler_first.generate(dmstore_shopid)
                 if sales_order_inss is None:
                     cursor_ai.execute(update_sql_02.format(taskflow.cal_status_failed, int(time.time() - start_time),
                                                            id))  # 更新到“结束计算”和耗时多少
@@ -49,12 +49,12 @@ def first_order_process():
                     cursor_ai.execute(select_goods_batch_order.format(batch_id,uc_shop_id))
                     goods_batch_data = cursor_ai.fetchone()
                     if goods_batch_data is None:
-                        inert_data = cacul_util.get_goods_batch_order_data(batch_id,sales_order_inss,uc_shop_id)
+                        inert_data = cacul_util.get_goods_batch_order_data(batch_id,sales_order_inss,uc_shop_id,result)
                         cursor_ai.executemany(insert_goods_batch_order, inert_data)
                         cursor_ai.connection.commit()
                     else:
-                        update_data = cacul_util.get_goods_batch_order_data(batch_id,sales_order_inss)
-                        cursor_ai.execute(update_goods_batch_order.format(update_data[0][1], update_data[0][3],goods_batch_data[0]))
+                        update_data = cacul_util.get_goods_batch_order_data(batch_id,sales_order_inss,result)
+                        cursor_ai.execute(update_goods_batch_order.format(update_data[0][1], update_data[0][3],update_data[0][5],goods_batch_data[0]))
                         cursor_ai.connection.commit()
                     # 更新数据库状态
                     cursor_ai.execute(
@@ -96,10 +96,10 @@ def day_order_process():
                 print("日常订单 batch_id =" + str(batch_id))
                 if dmstore_shopid in config.shellgoods_params["start_shop"].keys() and config.shellgoods_params["start_shop"][dmstore_shopid] > start_date:
                     print ("该店处于新店期，dmstore_shopid = "+str(dmstore_shopid))
-                    sales_order_inss1 = generate_order_2saler_newshop_notday.generate(dmstore_shopid)
+                    sales_order_inss1,result = generate_order_2saler_newshop_notday.generate(dmstore_shopid)
                 else:
-                    sales_order_inss1 = generate_order_2saler_add.generate(dmstore_shopid)
-                sales_order_inss2 = generate_order_2saler_add_day.generate(dmstore_shopid)
+                    sales_order_inss1,result = generate_order_2saler_add.generate(dmstore_shopid)
+                sales_order_inss2,result1 = generate_order_2saler_add_day.generate(dmstore_shopid)
                 if sales_order_inss1 is None or sales_order_inss2 is None:
                     cursor_ai.execute(update_sql_02.format(taskflow.cal_status_failed, int(time.time() - start_time),
                                                            id))  # 更新到“结束计算”和耗时多少
@@ -117,12 +117,12 @@ def day_order_process():
                     cursor_ai.execute(select_goods_batch_order.format(batch_id,uc_shop_id))
                     goods_batch_data = cursor_ai.fetchone()
                     if goods_batch_data is None:
-                        inert_data = cacul_util.get_goods_batch_order_data(batch_id,sales_order_inss,uc_shop_id)
+                        inert_data = cacul_util.get_goods_batch_order_data(batch_id,sales_order_inss,uc_shop_id,result1)
                         cursor_ai.executemany(insert_goods_batch_order, inert_data)
                         cursor_ai.connection.commit()
                     else:
-                        update_data = cacul_util.get_goods_batch_order_data(batch_id,sales_order_inss,uc_shop_id)
-                        cursor_ai.execute(update_goods_batch_order.format(update_data[0][1], update_data[0][3],goods_batch_data[0]))
+                        update_data = cacul_util.get_goods_batch_order_data(batch_id,sales_order_inss,uc_shop_id,result1)
+                        cursor_ai.execute(update_goods_batch_order.format(update_data[0][1], update_data[0][3],update_data[0][5],goods_batch_data[0]))
                         cursor_ai.connection.commit()
                     # 更新数据库状态
                     cursor_ai.execute(
@@ -160,7 +160,7 @@ def add_order_process():
                 conn.commit()
                 start_time = time.time()
                 print("补货单 batch_id =" + str(batch_id))
-                sales_order_inss = generate_order_shop.generate(dmstore_shopid)
+                sales_order_inss,result = generate_order_shop.generate(dmstore_shopid)
                 if sales_order_inss is None:
                     cursor_ai.execute(update_sql_02.format(taskflow.cal_status_failed, int(time.time() - start_time),
                                                         id))  # 更新到“结束计算”和耗时多少
@@ -171,12 +171,12 @@ def add_order_process():
                     cursor_ai.execute(select_goods_batch_order.format(batch_id,uc_shop_id))
                     goods_batch_data = cursor_ai.fetchone()
                     if goods_batch_data is None:
-                        inert_data = cacul_util.get_goods_batch_order_data(batch_id,sales_order_inss,uc_shop_id)
+                        inert_data = cacul_util.get_goods_batch_order_data(batch_id,sales_order_inss,uc_shop_id,result)
                         cursor_ai.executemany(insert_goods_batch_order, inert_data)
                         cursor_ai.connection.commit()
                     else:
-                        update_data = cacul_util.get_goods_batch_order_data(batch_id,sales_order_inss,uc_shop_id)
-                        cursor_ai.execute(update_goods_batch_order.format(update_data[0][1],update_data[0][3],goods_batch_data[0]))
+                        update_data = cacul_util.get_goods_batch_order_data(batch_id,sales_order_inss,uc_shop_id,result)
+                        cursor_ai.execute(update_goods_batch_order.format(update_data[0][1],update_data[0][3],update_data[0][5],goods_batch_data[0]))
                         cursor_ai.connection.commit()
                     # 更新数据库状态
                     cursor_ai.execute(
@@ -184,7 +184,7 @@ def add_order_process():
                     cursor_ai.connection.commit()
                     # 把订单结果和批次结果 通知给摩售
                     shop_type = config.shellgoods_params['shop_types'][0]  # 门店
-                    erp_interface.order_commit(dmstore_shopid,shop_type,sales_order_inss,batch_id=batch_id)
+                    # erp_interface.order_commit(dmstore_shopid,shop_type,sales_order_inss,batch_id=batch_id)
             except Exception as e:
                 cursor_ai.execute(update_sql_02.format(taskflow.cal_status_failed, 0,
                                                        data[0]))  # 更新到“结束计算”和耗时多少
