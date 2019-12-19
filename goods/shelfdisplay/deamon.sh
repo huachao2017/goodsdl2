ece401631db551f7f5ab9f5c6238738623d12361
huachao2017 <21006735@qq.com>
2019-12-19 16:00:09 +0800

    - 修订脚本

--- goods/shelfdisplay/deamon.sh
+++ goods/shelfdisplay/deamon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ps -ef | grep workflow_deamon | grep -v grep | awk '{print $2}' | xargs kill -9
-nohup python3 /home/src/goodsdl2/goods/shelfdisplay/workflow_deamon_process.py >> /home/src/goodsdl2/logs/shelfdisplay.log 2>&1 &
+nohup python3 -u /home/src/goodsdl2/goods/shelfdisplay/workflow_deamon_process.py >> /home/src/goodsdl2/logs/shelfdisplay.log 2>&1 &
