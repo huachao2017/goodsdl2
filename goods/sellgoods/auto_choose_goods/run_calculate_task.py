17b4b5b7be52455759029e7a79df8431cfbd4197
TreesLi <wlgcxy2012@163.com>
2019-12-19 18:29:24 +0800

    test

--- goods/sellgoods/auto_choose_goods/run_calculate_task.py
+++ goods/sellgoods/auto_choose_goods/run_calculate_task.py
@@ -3,9 +3,10 @@ import pymysql
 import datetime
 import os,django,time,math,sys
 from goods.util import SendEmail
-from goods.sellgoods.auto_choose_goods.haolinju_template import start_choose_goods
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
-# django.setup()
+# from goods.sellgoods.auto_choose_goods.haolinju_template import start_choose_goods
+from goods.sellgoods.auto_choose_goods.daily_change_goods import start_choose_goods
+
+
 import main.import_django_settings
 
 from goods.models import AllWorkFlowBatch
