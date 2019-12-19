a48cb11e77965c1c919d155cb741fff092080b85
huachao2017 <21006735@qq.com>
2019-12-19 18:10:21 +0800

    - 修订接口

--- goods/views_display.py
+++ goods/views_display.py
@@ -1,15 +1,14 @@
 import json
 import logging
-from rest_framework.response import Response
-from rest_framework import mixins
-from rest_framework import viewsets
-from rest_framework.views import APIView
-from rest_framework import status
-from goods.shelfdisplay.generate_shelf_display import generate_displays
-import requests
-from django.db import connections
-from .serializers import *
 
+import requests
+from rest_framework import mixins
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import *
 
 logger = logging.getLogger("django")
 
@@ -17,49 +16,3 @@ class ShelfDisplayDebugViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
                                viewsets.GenericViewSet):
     queryset = ShelfDisplayDebug.objects.order_by('-id')
     serializer_class = ShelfDisplayDebugSerializer
-
-class AutoDisplay(APIView):
-    def get(self,request):
-        tz_id = None
-        try:
-            uc_shopid = int(request.query_params['ucshopid'])
-            # if 'tzid' in request.query_params['tzid']:
-            tz_id = int(request.query_params['tzid'])
-        except Exception as e:
-            logger.error('Shelf auto display error:{}'.format(e))
-            return Response(-1, status=status.HTTP_400_BAD_REQUEST)
-
-        taizhangid_list = []
-        if tz_id is not None:
-            taizhangid_list.append(tz_id)
-        # else:
-        #     cursor = connections['ucenter'].cursor()
-        #
-        #     # 获取所有台账
-        #     try:
-        #         cursor.execute(
-        #             "select t.id, t.shelf_id, t.shelf_count, t.third_cate_ids from sf_shop_taizhang st, sf_taizhang t where st.taizhang_id=t.id and st.shop_id = {}".format(
-        #                 uc_shopid))
-        #         taizhang_infos = cursor.fetchall()
-        #         for taizhang_info in taizhang_infos:
-        #             taizhangid_list.append(taizhang_info[0])
-        #     except:
-        #         print('获取台账失败：{}！'.format(uc_shopid))
-        #         raise ValueError('taizhang error:{}'.format(uc_shopid))
-        #     finally:
-        #         cursor.close()
-
-        url = "https://autodisplay:xianlife2018@taizhang.aicvs.cn/api/autoDisplay"
-        headers = {
-            "Accept":"application/json",
-            "Content-Type":"application/json"
-        }
-        for taizhangid in taizhangid_list:
-            taizhang = generate_displays(uc_shopid, taizhangid)
-            json_info = json.dumps(taizhang.to_json())
-            data = bytes(json_info, 'utf8')
-            resp = requests.post(url=url, data=data, headers=headers)
-            print(resp)
-
-        return Response('OK', status=status.HTTP_200_OK)
-
