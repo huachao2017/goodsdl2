02a210c03ad9f06afe81647f5ae6ac37871a4723
TreesLi <wlgcxy2012@163.com>
2019-12-24 21:04:16 +0800

    test

--- goods/sellgoods/auto_choose_goods/choosed_goods_out.py
+++ goods/sellgoods/auto_choose_goods/choosed_goods_out.py
@@ -26,7 +26,7 @@ def goods_out(uc_shopid,template_shop_ids,batch_id,days):
     cursor_ai = conn_ai.cursor()
     # conn_dmstore = connections['dmstore']
     # cursor_dmstore = conn_dmstore.cursor()
-    print("时间,门店id,门店名称,一级分类,二级分类,三级分类,配送类型,商品编码,商品名称,商品upc,策略标签,商品角色,上品优先级排名,商品实际销售4周预期psd金额,商品实际销售4周预期psd,组内门店4周预期psd金额,组内门店4周预期psd")
+    print("时间,门店id,门店名称,一级分类,二级分类,三级分类,四级分类,配送类型,商品编码,商品名称,商品upc,策略标签,商品角色,上品优先级排名,商品实际销售4周预期psd金额,商品实际销售4周预期psd,组内门店4周预期psd金额,组内门店4周预期psd")
 
 
     select_sql = "select * from goods_goodsselectionhistory where uc_shopid={} and batch_id='{}'"
@@ -62,8 +62,8 @@ def goods_out(uc_shopid,template_shop_ids,batch_id,days):
             line_str += str('None')  # 门店名称
         line_str += ","
 
-        # class_type_sql = "select category1_id,category2_id,category_id,delivery_type from uc_merchant_goods a where mch_goods_code={} and delivery_type is not Null"
-        class_type_sql = "SELECT DISTINCT first_cate_id,second_cate_id,third_cate_id from goods WHERE neighbor_goods_id ={} AND corp_id=2"
+        class_type_sql = "select display_first_cat_id,display_second_cat_id,display_third_cat_id,display_fourth_cat_id from uc_merchant_goods a where mch_goods_code={} and delivery_type is not Null"
+        # class_type_sql = "SELECT DISTINCT first_cate_id,second_cate_id,third_cate_id from goods WHERE neighbor_goods_id ={} AND corp_id=2"
         cursor_dmstore.execute(class_type_sql.format(data[10]))
         class_type_data = cursor_dmstore.fetchone()
         # print('分类',data[10],class_type_data)
@@ -86,8 +86,16 @@ def goods_out(uc_shopid,template_shop_ids,batch_id,days):
             line_str += str('None')
             line_str += ","
 
+        try:
+            line_str += str(class_type_data[3])  # 四级分类
+            line_str += ","
+        except:
+            line_str += str('None')
+            line_str += ","
+
         delivery_type_dict = {1:'日配',2:'非日配','1':'日配','2':'非日配'}
         try:
+            print(data[21])
             line_str += str(delivery_type_dict[data[21]])  # 配送类型
             line_str += ","
         except:
