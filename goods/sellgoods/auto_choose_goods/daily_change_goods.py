49282e72e8229fb7c08885ecb217252a42da8ab9
TreesLi <wlgcxy2012@163.com>
2019-12-25 12:38:34 +0800

    test

--- goods/sellgoods/auto_choose_goods/daily_change_goods.py
+++ goods/sellgoods/auto_choose_goods/daily_change_goods.py
@@ -71,6 +71,7 @@ class DailyChangeGoods:
         else:
             print('none', shop_ids,type(shop_ids))
             return []
+        print('sql',sql)
         self.cursor.execute(sql.format(week_ago, now_date, shop_ids,third_category))
         results = self.cursor.fetchall()
         # cursor.close()
