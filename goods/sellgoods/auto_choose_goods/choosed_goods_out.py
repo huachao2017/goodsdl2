f290e22c70e7eb20a838f33e5744e364267526cb
TreesLi <wlgcxy2012@163.com>
2019-12-26 17:22:40 +0800

    test

--- goods/sellgoods/auto_choose_goods/choosed_goods_out.py
+++ goods/sellgoods/auto_choose_goods/choosed_goods_out.py
@@ -29,7 +29,7 @@ def goods_out(uc_shopid,template_shop_ids,batch_id,days):
     print("时间,门店id,门店名称,一级分类,二级分类,三级分类,四级分类,配送类型,商品编码,商品名称,商品upc,策略标签,商品角色,上品优先级排名,商品实际销售4周预期psd金额,商品实际销售4周预期psd,组内门店4周预期psd金额,组内门店4周预期psd")
 
 
-    select_sql = "select * from goods_goodsselectionhistory where uc_shopid={} and batch_id='{}' and upc is not NULL and goods_role in (1,3,0,2,4)"
+    select_sql = "select * from goods_goodsselectionhistory where uc_shopid={} and batch_id='{}' and upc is not NULL and goods_role in (1,3)"
     select_sql_02 = "select mch_goods_code from goods_goodsselectionhistory where uc_shopid={} and batch_id='{}' and upc is NULL and goods_role=3"
     cursor_ai.execute(select_sql.format(uc_shopid,batch_id))
     all_data = cursor_ai.fetchall()
