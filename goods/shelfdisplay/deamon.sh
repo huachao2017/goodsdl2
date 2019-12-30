38cd92538ce6e2d892feecbc8ed51930d0186dbd
huachao2017 <21006735@qq.com>
2019-12-30 10:45:08 +0800

    - 修订计算地址

--- goods/shelfdisplay/deamon.sh
+++ goods/shelfdisplay/deamon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 today=`date +"%Y-%m-%d"`
 ps -ef | grep workflow_deamon | grep -v grep | awk '{print $2}' | xargs kill -9
-nohup python3 -u /home/src/goodsdl2/goods/shelfdisplay/workflow_deamon_process.py >> /home/src/goodsdl2/logs/shelfdisplay.log""$today 2>&1 &
+nohup python3 -u /data/src/goodsdl2/goods/shelfdisplay/workflow_deamon_process.py >> /data/src/goodsdl2/logs/shelfdisplay.log""$today 2>&1 &
