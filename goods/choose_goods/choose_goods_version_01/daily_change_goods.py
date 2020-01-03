96948eda2cb165e3803e85d127884114d01de9fd
TreesLi <wlgcxy2012@163.com>
2020-01-03 17:26:02 +0800

    test

--- goods/choose_goods/choose_goods_version_01/daily_change_goods.py
+++ goods/choose_goods/choose_goods_version_01/daily_change_goods.py
@@ -46,7 +46,7 @@ class DailyChangeGoods:
 
         # conn = pymysql.connect('123.103.16.19', 'readonly', password='fxiSHEhui2018@)@)', database='dmstore',charset="utf8", port=3300, use_unicode=True)
         # self.cursor = conn.cursor()
-        close_old_connections()
+        # close_old_connections()
         self.cursor = connections['dmstore'].cursor()
         self.cursor_ucenter = connections['ucenter'].cursor()
 
@@ -142,7 +142,7 @@ class DailyChangeGoods:
         # 注意：and c.display_third_cat_id>0 类型是字符串
         sql = "select c.display_third_cat_id,GROUP_CONCAT(a.supplier_goods_code) from uc_supplier_goods a LEFT JOIN uc_merchant_goods c on a.supplier_goods_code=c.mch_goods_code where a.supplier_id in ({}) and a.order_status = 1 and c.width > 0 and c.height > 0 and c.depth > 0 and c.mch_goods_code in ({}) and c.display_third_cat_id>0 GROUP BY c.display_third_cat_id"
         try:
-            print("sql语句：",self.supplier_id_list)
+            # print("sql语句：",self.supplier_id_list)
 
             print(sql.format(','.join(self.supplier_id_list),",".join(mch_code_list)))
             self.cursor_ucenter.execute(sql.format(','.join(self.supplier_id_list),",".join(mch_code_list)))
