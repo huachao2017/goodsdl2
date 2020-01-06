8f3b9643008014b3fd3b000c11cab040ceefeebe
TreesLi <wlgcxy2012@163.com>
2020-01-06 13:54:28 +0800

    关联品分值入库2

--- goods/choose_goods/choose_goods_version_01/daily_change_goods.py
+++ goods/choose_goods/choose_goods_version_01/daily_change_goods.py
@@ -720,8 +720,6 @@ class DailyChangeGoods:
 
 
 
-
-
 def start_choose_goods(batch_id,uc_shop_id,pos_shopid):
     # f = DailyChangeGoods(pos_shopid, "88,3156,3238",batch_id,uc_shop_id)
     f = DailyChangeGoods(pos_shopid, "1284,3955,3779,1925,4076,1924,3598",batch_id,uc_shop_id)
