cdcfac6cf7de27c47fe69402f5c9222224560e67
TreesLi <wlgcxy2012@163.com>
2020-01-10 17:42:56 +0800

    -

--- goods/choose_goods/choose_goods_version_02/daily_change_goods.py
+++ goods/choose_goods/choose_goods_version_02/daily_change_goods.py
@@ -764,6 +764,7 @@ def start_choose_goods(batch_id,uc_shop_id,pos_shopid):
 
 
 
+
 if __name__ == '__main__':
 
     # f = DailyChangeGoods(1284, "1284,3955,3779,1925,4076,1924,3598,223,4004",'lishu_test_011',806)
