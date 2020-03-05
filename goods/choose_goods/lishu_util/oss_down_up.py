d2d64ac478a9d07824d4590f11a333db8bc6b7f8
TreesLi <wlgcxy2012@163.com>
2020-03-05 15:34:25 +0800

    oss对象存储--流数据存储

--- goods/choose_goods/lishu_util/oss_down_up.py
+++ goods/choose_goods/lishu_util/oss_down_up.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # api说明：https://github.com/ucloud/ufile-sdk-python
 from ufile import filemanager
+from io import BytesIO
+
 
 class FileManager:
     """
@@ -68,13 +70,35 @@ class FileManager:
         ret, resp = putufile_handler.putfile(public_bucket, put_key, localfile_path_name, header=None)
         print(resp.status_code)
         # assert resp.status_code == 200
-        #
-        # # 普通上传二进制数据流至公共空间
-        # from io import BytesIO
-        # bio = BytesIO(u'Do be a good man'.encode('utf-8'))  # 二进制数据流
-        # stream_key = ''  # 上传数据流在空间中的名称
-        # ret, resp = putufile_handler.putstream(public_bucket, stream_key, bio)
+        if resp.status_code == 200:
+            return True
+        else:
+            return False
 
+    def stream_upload(self,ucloud_file_name,stream):
+        """
+        文件上传
+        ucloud_file_name: 在ucloud的名字
+        localfile_path_name：在本地的路径和名字
+        状态码描述：
+            200：文件或者数据上传成功
+            400：上传到不存在的空间
+            403：API公私钥错误
+            401：上传凭证错误
+        :return:
+        """
+        public_bucket = 'louxia'  # 公共空间名称
+        # localfile = 'C:/Users/86130/Pictures/BlueDream_4k.jpg'  # 本地文件名
+
+        putufile_handler = filemanager.FileManager(self.public_key, self.private_key)
+
+        # 普通上传二进制数据流至公共空间
+        # with open(localfile_path_name, 'rb') as f:
+        #     a = f.read()
+        bio = BytesIO(stream)  # 二进制数据流
+        stream_key = 'store_ass/{}'.format(ucloud_file_name)  # 上传数据流在空间中的名称
+        ret, resp = putufile_handler.putstream(public_bucket, stream_key, bio)
+        print(resp.status_code)
         if resp.status_code == 200:
             return True
         else:
@@ -106,4 +130,5 @@ class FileManager:
 
 if __name__ == '__main__':
     m = FileManager()
-    # m.download('a.jpg','C:/programs/goodsdl2/goods/choose_goods/lishu_util/w.jpg')
+    # m.upload('demo1.jpg','C:/programs/goodsdl2/goods/choose_goods/lishu_util/demo.jpg',True)
+    m.download('demo1.jpg','C:/programs/goodsdl2/goods/choose_goods/lishu_util/q1.jpg')
