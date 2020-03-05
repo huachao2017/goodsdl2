f3737baeb65bdc009f75c0c0dd6977e1b3445f4d
LRJliurj <908601417@qq.com>
2020-03-05 14:06:36 +0800

    dd

--- set_config/test.py
+++ set_config/test.py
@@ -29,9 +29,9 @@ mengniu_yolov3_params={
 #########################################YOLOV3-shelf##################################################################
 shelf_yolov3_params={
     'good_model_path' :'/home/ai/model/shelf/ep1360-loss7.398-val_loss8.272.h5',
-    'anchors_path' :'./goods/freezer/keras_yolo3/model_data/shelf_yolo_anchors.txt',
-    'classes_path' : './goods/freezer/keras_yolo3/model_data/shelf_voc_classes.txt',
-    'label_path':'./goods/freezer/keras_yolo3/model_data/shelf_label_map.pbtxt',
+    'anchors_path' :'/home/src/goodsdl2/goods/freezer/keras_yolo3/model_data/shelf_yolo_anchors.txt',
+    'classes_path' : '/home/src/goodsdl2/goods/freezer/keras_yolo3/model_data/shelf_voc_classes.txt',
+    'label_path':'/home/src/goodsdl2/goods/freezer/keras_yolo3/model_data/shelf_label_map.pbtxt',
     'score' :0.2,
     'iou' :0.45,
     'model_image_size' : (416, 416),
