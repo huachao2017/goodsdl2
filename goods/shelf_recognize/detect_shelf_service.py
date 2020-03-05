1625daea04058d006cdd2de69f6c21eb5dcadc5e
LRJliurj <908601417@qq.com>
2020-03-05 15:55:21 +0800

    dd

--- goods/shelf_recognize/detect_shelf_service.py
+++ goods/shelf_recognize/detect_shelf_service.py
@@ -17,7 +17,7 @@ class DetectShelf:
         'shelf':0.6,
         'null_box':0.2,
     }
-    sql1 = "select id,origin_img_url,ai_status,vacancy_face_num,ai_img_url from sf_shop_shelf_everyday where ai_status = 0 "
+    sql1 = "select id,origin_img_url,ai_status,vacancy_face_num,ai_img_url,ai_result_desc from sf_shop_shelf_everyday where ai_status = 0 "
     sql2 = "update sf_shop_shelf_everyday set ai_status = %s,vacancy_face_num = %s,ai_img_url = %s,updated_at = %s where id = %s "
     def detect(self):
         while True:
@@ -39,28 +39,59 @@ class DetectShelf:
                         ai_status = ret[2]
                         vacancy_face_num = ret[3]
                         ai_img_url = ret[4]
+                        ai_result_desc = ret[5]
+                        down_http_file = origin_img_url
+                        ai_filename = "".join(str(uuid.uuid4()).split("-")).lower()+".jpg"
+                        down_local_file = os.path.join(down_jpg, ai_filename)
                         try:
-                            down_http_file = origin_img_url
-                            ai_filename = "".join(str(uuid.uuid4()).split("-")).lower()+".jpg"
-                            down_local_file = os.path.join(down_jpg, ai_filename)
+                            # 下载文件
                             file_oss_ins.download(down_http_file,down_local_file)
-                            up_local_file = os.path.join(down_jpg,"vis_"+ai_filename)
+                        except:
+                            print("shelf bull_box detect error id = {},origin_img_url={},down jpg failed".format(id, origin_img_url))
+                            traceback.print_exc()
+                            ai_result_desc = "ufile down failed"
+                            ai_status = 2
+                            ai_img_url = ''
+                            vacancy_face_num = -1,
+                            updated_at = str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+                            data.append((ai_status, vacancy_face_num, ai_img_url, updated_at,ai_result_desc, id))
+                            continue
+                        up_local_file = os.path.join(down_jpg, "vis_" + ai_filename)
+                        null_box_num = -1
+                        try:
                             ret, detect_time, output_image, null_box_num = shelf_yolo_ins.detect(down_local_file,self.default_params)
                             output_image.save(up_local_file)
-                            file_oss_ins.upload("vis_"+ai_filename, up_local_file)
-                            vacancy_face_num = null_box_num
-                            ai_status = 1
-                            ai_img_url = "vis_"+ai_filename
-                            updated_at = str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-                            data.append((ai_status,vacancy_face_num,ai_img_url,updated_at,id))
-                        except:
-                            print("shelf bull_box detect error id = {},origin_img_url={}".format(id,origin_img_url))
+                        except Exception as e :
+                            print("shelf bull_box detect error id = {},origin_img_url={},down_local_file={},check jpg failed".format(id,
+                                                                                                                 origin_img_url,down_local_file))
                             traceback.print_exc()
-                            ai_status=2
-                            ai_img_url=''
-                            vacancy_face_num=-1,
+                            ai_result_desc = "ufile check failed,e={}".format(e)
+                            ai_status = 2
+                            ai_img_url = ''
+                            vacancy_face_num = -1,
                             updated_at = str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-                            data.append((ai_status,vacancy_face_num,ai_img_url,updated_at,id))
+                            data.append((ai_status, vacancy_face_num, ai_img_url, updated_at, ai_result_desc, id))
+                            continue
+
+                        try:
+                            file_oss_ins.upload("vis_"+ai_filename, up_local_file)
+                        except:
+                            print("shelf bull_box detect error id = {},origin_img_url={},upload jpg failed,up_local_file={}".format(id,
+                                                                                                                  origin_img_url,up_local_file))
+                            traceback.print_exc()
+                            ai_result_desc = "ufile upload failed"
+                            ai_status = 2
+                            ai_img_url = ''
+                            vacancy_face_num = -1,
+                            updated_at = str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+                            data.append((ai_status, vacancy_face_num, ai_img_url, updated_at, ai_result_desc, id))
+                            continue
+                        vacancy_face_num = null_box_num
+                        ai_status = 1
+                        ai_img_url = "vis_" + ai_filename
+                        ai_result_desc=''
+                        updated_at = str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+                        data.append((ai_status, vacancy_face_num, ai_img_url, updated_at, ai_result_desc,id))
                     cursor_aucenter.executemany(self.sql2, data)
                     conn.commit()
                 cursor_aucenter.close()
